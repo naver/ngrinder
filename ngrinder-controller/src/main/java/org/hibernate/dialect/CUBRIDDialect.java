e686a4e3f6543a88dafac1f43ff0f344f3c288ed
JunHo Yoon <junoyoon@gmail.com>
2012-06-22 11:54:57 +0900

    [NGRINDER-18] Move database configuration fron ngrinder web app folder to ${NGRINDER_HOME}/database.conf.

--- ngrinder-controller/src/main/java/org/hibernate/dialect/CUBRIDDialect.java
+++ ngrinder-controller/src/main/java/org/hibernate/dialect/CUBRIDDialect.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2012 - 2012 NHN Corporation
+ * All rights reserved.
+ *
+ * This file is part of The nGrinder software distribution. Refer to
+ * the file LICENSE which is part of The nGrinder distribution for
+ * licensing details. The nGrinder distribution is available on the
+ * Internet at http://nhnopensource.org/ngrinder
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.hibernate.dialect;
 
 import java.sql.Types;
@@ -9,6 +31,12 @@ import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.dialect.function.VarArgsSQLFunction;
 import org.hibernate.type.StandardBasicTypes;
 
+/**
+ * Hibernate CUBRID Dialect
+ * 
+ * @author JunHo Yoon
+ * @since 3.0
+ */
 public class CUBRIDDialect extends Dialect {
 	@Override
 	protected String getIdentityColumnString() throws MappingException {
@@ -16,8 +44,7 @@ public class CUBRIDDialect extends Dialect {
 	}
 
 	@Override
-	public String getIdentitySelectString(String table, String column, int type)
-			throws MappingException {
+	public String getIdentitySelectString(String table, String column, int type) throws MappingException {
 		return "select last_insert_id()";
 	}
 
@@ -43,18 +70,13 @@ public class CUBRIDDialect extends Dialect {
 		registerColumnType(Types.BLOB, "blob");
 		registerColumnType(Types.CLOB, "string");
 
-		getDefaultProperties().setProperty(Environment.USE_STREAMS_FOR_BINARY,
-				"true");
-		getDefaultProperties().setProperty(Environment.STATEMENT_BATCH_SIZE,
-				DEFAULT_BATCH_SIZE);
+		getDefaultProperties().setProperty(Environment.USE_STREAMS_FOR_BINARY, "true");
+		getDefaultProperties().setProperty(Environment.STATEMENT_BATCH_SIZE, DEFAULT_BATCH_SIZE);
 
-		registerFunction("substring", new StandardSQLFunction("substr",
-				StandardBasicTypes.STRING));
+		registerFunction("substring", new StandardSQLFunction("substr", StandardBasicTypes.STRING));
 		registerFunction("trim", new StandardSQLFunction("trim"));
-		registerFunction("length", new StandardSQLFunction("length",
-				StandardBasicTypes.INTEGER));
-		registerFunction("bit_length", new StandardSQLFunction("bit_length",
-				StandardBasicTypes.INTEGER));
+		registerFunction("length", new StandardSQLFunction("length", StandardBasicTypes.INTEGER));
+		registerFunction("bit_length", new StandardSQLFunction("bit_length", StandardBasicTypes.INTEGER));
 		registerFunction("coalesce", new StandardSQLFunction("coalesce"));
 		registerFunction("nullif", new StandardSQLFunction("nullif"));
 		registerFunction("abs", new StandardSQLFunction("abs"));
@@ -73,55 +95,32 @@ public class CUBRIDDialect extends Dialect {
 		registerFunction("rtrim", new StandardSQLFunction("rtrim"));
 		registerFunction("nvl", new StandardSQLFunction("nvl"));
 		registerFunction("nvl2", new StandardSQLFunction("nvl2"));
-		registerFunction("sign", new StandardSQLFunction("sign",
-				StandardBasicTypes.INTEGER));
-		registerFunction("chr", new StandardSQLFunction("chr",
-				StandardBasicTypes.CHARACTER));
-		registerFunction("to_char", new StandardSQLFunction("to_char",
-				StandardBasicTypes.STRING));
-		registerFunction("to_date", new StandardSQLFunction("to_date",
-				StandardBasicTypes.TIMESTAMP));
-		registerFunction("last_day", new StandardSQLFunction("last_day",
-				StandardBasicTypes.DATE));
-		registerFunction("instr", new StandardSQLFunction("instr",
-				StandardBasicTypes.INTEGER));
-		registerFunction("instrb", new StandardSQLFunction("instrb",
-				StandardBasicTypes.INTEGER));
-		registerFunction("lpad", new StandardSQLFunction("lpad",
-				StandardBasicTypes.STRING));
-		registerFunction("replace", new StandardSQLFunction("replace",
-				StandardBasicTypes.STRING));
-		registerFunction("rpad", new StandardSQLFunction("rpad",
-				StandardBasicTypes.STRING));
-		registerFunction("substr", new StandardSQLFunction("substr",
-				StandardBasicTypes.STRING));
-		registerFunction("substrb", new StandardSQLFunction("substrb",
-				StandardBasicTypes.STRING));
-		registerFunction("translate", new StandardSQLFunction("translate",
-				StandardBasicTypes.STRING));
-		registerFunction("add_months", new StandardSQLFunction("add_months",
-				StandardBasicTypes.DATE));
-		registerFunction("months_between", new StandardSQLFunction(
-				"months_between", StandardBasicTypes.FLOAT));
+		registerFunction("sign", new StandardSQLFunction("sign", StandardBasicTypes.INTEGER));
+		registerFunction("chr", new StandardSQLFunction("chr", StandardBasicTypes.CHARACTER));
+		registerFunction("to_char", new StandardSQLFunction("to_char", StandardBasicTypes.STRING));
+		registerFunction("to_date", new StandardSQLFunction("to_date", StandardBasicTypes.TIMESTAMP));
+		registerFunction("last_day", new StandardSQLFunction("last_day", StandardBasicTypes.DATE));
+		registerFunction("instr", new StandardSQLFunction("instr", StandardBasicTypes.INTEGER));
+		registerFunction("instrb", new StandardSQLFunction("instrb", StandardBasicTypes.INTEGER));
+		registerFunction("lpad", new StandardSQLFunction("lpad", StandardBasicTypes.STRING));
+		registerFunction("replace", new StandardSQLFunction("replace", StandardBasicTypes.STRING));
+		registerFunction("rpad", new StandardSQLFunction("rpad", StandardBasicTypes.STRING));
+		registerFunction("substr", new StandardSQLFunction("substr", StandardBasicTypes.STRING));
+		registerFunction("substrb", new StandardSQLFunction("substrb", StandardBasicTypes.STRING));
+		registerFunction("translate", new StandardSQLFunction("translate", StandardBasicTypes.STRING));
+		registerFunction("add_months", new StandardSQLFunction("add_months", StandardBasicTypes.DATE));
+		registerFunction("months_between", new StandardSQLFunction("months_between", StandardBasicTypes.FLOAT));
 
-		registerFunction("current_date", new NoArgSQLFunction("current_date",
-				StandardBasicTypes.DATE, false));
-		registerFunction("current_time", new NoArgSQLFunction("current_time",
-				StandardBasicTypes.TIME, false));
-		registerFunction("current_timestamp", new NoArgSQLFunction(
-				"current_timestamp", StandardBasicTypes.TIMESTAMP, false));
-		registerFunction("sysdate", new NoArgSQLFunction("sysdate",
-				StandardBasicTypes.DATE, false));
-		registerFunction("systime", new NoArgSQLFunction("systime",
-				StandardBasicTypes.TIME, false));
-		registerFunction("systimestamp", new NoArgSQLFunction("systimestamp",
-				StandardBasicTypes.TIMESTAMP, false));
-		registerFunction("user", new NoArgSQLFunction("user",
-				StandardBasicTypes.STRING, false));
-		registerFunction("rownum", new NoArgSQLFunction("rownum",
-				StandardBasicTypes.LONG, false));
-		registerFunction("concat", new VarArgsSQLFunction(
-				StandardBasicTypes.STRING, "", "||", ""));
+		registerFunction("current_date", new NoArgSQLFunction("current_date", StandardBasicTypes.DATE, false));
+		registerFunction("current_time", new NoArgSQLFunction("current_time", StandardBasicTypes.TIME, false));
+		registerFunction("current_timestamp", new NoArgSQLFunction("current_timestamp", StandardBasicTypes.TIMESTAMP,
+				false));
+		registerFunction("sysdate", new NoArgSQLFunction("sysdate", StandardBasicTypes.DATE, false));
+		registerFunction("systime", new NoArgSQLFunction("systime", StandardBasicTypes.TIME, false));
+		registerFunction("systimestamp", new NoArgSQLFunction("systimestamp", StandardBasicTypes.TIMESTAMP, false));
+		registerFunction("user", new NoArgSQLFunction("user", StandardBasicTypes.STRING, false));
+		registerFunction("rownum", new NoArgSQLFunction("rownum", StandardBasicTypes.LONG, false));
+		registerFunction("concat", new VarArgsSQLFunction(StandardBasicTypes.STRING, "", "||", ""));
 	}
 
 	public String getAddColumnString() {
@@ -158,8 +157,8 @@ public class CUBRIDDialect extends Dialect {
 
 	public String getLimitString(String sql, boolean hasOffset) {
 		// CUBRID 8.3.0 support limit
-		return new StringBuffer(sql.length() + 20).append(sql)
-				.append(hasOffset ? " limit ?, ?" : " limit ?").toString();
+		return new StringBuffer(sql.length() + 20).append(sql).append(hasOffset ? " limit ?, ?" : " limit ?")
+				.toString();
 	}
 
 	public boolean useMaxForLimit() {
---
+++ ngrinder-controller/src/main/java/org/hibernate/dialect/SQLiteDialect.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2012 - 2012 NHN Corporation
+ * All rights reserved.
+ *
+ * This file is part of The nGrinder software distribution. Refer to
+ * the file LICENSE which is part of The nGrinder distribution for
+ * licensing details. The nGrinder distribution is available on the
+ * Internet at http://nhnopensource.org/ngrinder
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hibernate.dialect;
+
+import java.sql.Types;
+
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.dialect.function.VarArgsSQLFunction;
+import org.hibernate.type.StandardBasicTypes;
+
+/**
+ * Hibernate SQLite Dialect
+ * 
+ * @author JunHo Yoon
+ * @since 3.0
+ */
+public class SQLiteDialect extends Dialect {
+	public SQLiteDialect() {
+		super();
+		registerColumnType(Types.BIT, "integer");
+		registerColumnType(Types.TINYINT, "tinyint");
+		registerColumnType(Types.SMALLINT, "smallint");
+		registerColumnType(Types.INTEGER, "integer");
+		registerColumnType(Types.BIGINT, "bigint");
+		registerColumnType(Types.FLOAT, "float");
+		registerColumnType(Types.REAL, "real");
+		registerColumnType(Types.DOUBLE, "double");
+		registerColumnType(Types.NUMERIC, "numeric");
+		registerColumnType(Types.DECIMAL, "decimal");
+		registerColumnType(Types.CHAR, "char");
+		registerColumnType(Types.VARCHAR, "varchar");
+		registerColumnType(Types.LONGVARCHAR, "longvarchar");
+		registerColumnType(Types.DATE, "date");
+		registerColumnType(Types.TIME, "time");
+		registerColumnType(Types.TIMESTAMP, "timestamp");
+		registerColumnType(Types.BINARY, "blob");
+		registerColumnType(Types.VARBINARY, "blob");
+		registerColumnType(Types.LONGVARBINARY, "blob");
+		// registerColumnType(Types.NULL, "null");
+		registerColumnType(Types.BLOB, "blob");
+		registerColumnType(Types.CLOB, "clob");
+		registerColumnType(Types.BOOLEAN, "integer");
+
+		registerFunction("concat", new VarArgsSQLFunction(StandardBasicTypes.STRING, "", "||", ""));
+		registerFunction("mod", new SQLFunctionTemplate(StandardBasicTypes.INTEGER, "?1 % ?2"));
+		registerFunction("substr", new StandardSQLFunction("substr", StandardBasicTypes.STRING));
+		registerFunction("substring", new StandardSQLFunction("substr", StandardBasicTypes.STRING));
+	}
+
+	public boolean supportsIdentityColumns() {
+		return true;
+	}
+
+	/*
+	 * public boolean supportsInsertSelectIdentity() { return true; // As
+	 * specify in NHibernate dialect }
+	 */
+
+	public boolean hasDataTypeInIdentityColumn() {
+		return false; // As specify in NHibernate dialect
+	}
+
+	/*
+	 * public String appendIdentitySelectToInsert(String insertString) { return
+	 * new StringBuffer(insertString.length()+30). // As specify in NHibernate
+	 * dialect append(insertString).
+	 * append("; ").append(getIdentitySelectString()). toString(); }
+	 */
+
+	public String getIdentityColumnString() {
+		// return "integer primary key autoincrement";
+		return "integer";
+	}
+
+	public String getIdentitySelectString() {
+		return "select last_insert_rowid()";
+	}
+
+	public boolean supportsLimit() {
+		return true;
+	}
+
+	public String getLimitString(String query, boolean hasOffset) {
+		return new StringBuffer(query.length() + 20).append(query).append(hasOffset ? " limit ? offset ?" : " limit ?")
+				.toString();
+	}
+
+	public boolean supportsTemporaryTables() {
+		return true;
+	}
+
+	public String getCreateTemporaryTableString() {
+		return "create temporary table if not exists";
+	}
+
+	public boolean dropTemporaryTableAfterUse() {
+		return false;
+	}
+
+	public boolean supportsCurrentTimestampSelection() {
+		return true;
+	}
+
+	public boolean isCurrentTimestampSelectStringCallable() {
+		return false;
+	}
+
+	public String getCurrentTimestampSelectString() {
+		return "select current_timestamp";
+	}
+
+	public boolean supportsUnionAll() {
+		return true;
+	}
+
+	public boolean hasAlterTable() {
+		return false; // As specify in NHibernate dialect
+	}
+
+	public boolean dropConstraints() {
+		return false;
+	}
+
+	public String getAddColumnString() {
+		return "add column";
+	}
+
+	public String getForUpdateString() {
+		return "";
+	}
+
+	public boolean supportsOuterJoinForUpdate() {
+		return false;
+	}
+
+	public String getDropForeignKeyString() {
+		throw new UnsupportedOperationException("No drop foreign key syntax supported by SQLiteDialect");
+	}
+
+	public String getAddForeignKeyConstraintString(String constraintName, String[] foreignKey, String referencedTable,
+			String[] primaryKey, boolean referencesPrimaryKey) {
+		throw new UnsupportedOperationException("No add foreign key syntax supported by SQLiteDialect");
+	}
+
+	public String getAddPrimaryKeyConstraintString(String constraintName) {
+		throw new UnsupportedOperationException("No add primary key syntax supported by SQLiteDialect");
+	}
+
+	public boolean supportsIfExistsBeforeTableName() {
+		return true;
+	}
+
+	public boolean supportsCascadeDelete() {
+		return false;
+	}
+}
