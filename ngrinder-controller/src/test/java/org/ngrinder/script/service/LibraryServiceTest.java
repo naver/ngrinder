cd0fd0984e733db9437a11295b775a96a46817b9
JunHo Yoon <junoyoon@gmail.com>
2012-07-02 01:07:07 +0900

    [NGRINDER-21] * Add missing merged part.

--- ngrinder-controller/src/test/java/org/ngrinder/script/service/LibraryServiceTest.java
+++ ngrinder-controller/src/test/java/org/ngrinder/script/service/LibraryServiceTest.java
@@ -70,7 +70,12 @@ public class LibraryServiceTest extends NGrinderIocTransactionalTestBase {
 		this.saveLibrary("get4");
 
 		User user = new User();
+		user.setId(123);
+		user.setName("tmp_user9");
+		UserUtil.setCurrentUser(user);
+		List<Library> libraries = libraryService.getLibraries();
 
+		Assert.assertEquals(0, libraries.size());
 	}
 
 	private Library saveLibrary(String key) {
