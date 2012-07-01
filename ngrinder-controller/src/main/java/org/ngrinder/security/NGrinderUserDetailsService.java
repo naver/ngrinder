09aed561e6edb0bc40a59a6f9aa89a8ab3428764
JunHo Yoon <junoyoon@gmail.com>
2012-07-02 04:39:55 +0900

    [NGRINDER-23] add spring security infra.. Not yet completed.

---
+++ ngrinder-controller/src/main/java/org/ngrinder/security/NGrinderUserDetailsService.java
@@ -0,0 +1,27 @@
+package org.ngrinder.security;
+
+import org.ngrinder.user.model.SecuredUser;
+import org.ngrinder.user.model.User;
+import org.ngrinder.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class NGrinderUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserService userService;
+
+	@Override
+	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException, DataAccessException {
+		User user = userService.getUserById(userId);
+		if (user != null) {
+			return new SecuredUser(user);
+		} else {
+			return null;
+		}
+	}
+
+}
