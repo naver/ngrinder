067dcaece3d2c6872d7498f414d31929860fe4a7
JunHo Yoon <junoyoon@nhn.com>
2012-07-11 16:11:50 +0900

    [NGRINDER-42] UI element cleanup

--- ngrinder-controller/src/main/java/org/ngrinder/infra/spring/UserHandlerMethodArgumentResolver.java
+++ ngrinder-controller/src/main/java/org/ngrinder/infra/spring/UserHandlerMethodArgumentResolver.java
@@ -2,18 +2,15 @@ package org.ngrinder.infra.spring;
 
 import org.ngrinder.model.User;
 import org.ngrinder.user.service.UserContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-@Component("userHandlerMethodArgumentResolver")
+
 public class UserHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
-	@Autowired
 	private UserContext userContext;
 
 	@Override
