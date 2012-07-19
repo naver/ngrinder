ab6d5f94309a17fd508d1879db7e7f831a5f4cce
Mavlarn <mavlarn@gmail.com>
2012-07-19 17:37:21 +0800

    [NGRINDER-12] Custom dns resolution
    Added necessary META-INF file, and add some test code and comment to
    understand how it works.

--- ngrinder-core/src/test/java/org/ngrinder/dns/LocalManagedDnsTest.java
+++ ngrinder-core/src/test/java/org/ngrinder/dns/LocalManagedDnsTest.java
@@ -4,14 +4,23 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.security.Security;
 
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+/**
+ * 
+ * To test the custom DNS, should to add vm arguments as below:
+	-Dsun.net.spi.nameservice.provider.1=dns,LocalManagedDns
+	-Dngrinder.etc.hosts=10.0.0.1:www.google.com,10.0.0.2:www.google2.com
+ *
+ * @author Mavlarn
+ * @since
+ */
 public class LocalManagedDnsTest {
+	
+	LocalManagedDns localNS = new LocalManagedDns();
 
-	@Before
-	public void before() {
+	public void beforeCustomDNS() {
 		// enable local DNS
 		System.setProperty("sun.net.spi.nameservice.provider.1", "dns," + LocalManagedDnsDescriptor.DNS_PROVIDER_NAME);
 		System.setProperty("sun.net.spi.nameservice.provider.2", "dns,sun");
@@ -19,19 +28,73 @@ public class LocalManagedDnsTest {
 		// disable DNS cache.
 		// Cache should be enabled later..
 		Security.setProperty("networkaddress.cache.ttl", "0");
+	}
+	
+	public void cleanCustomDNS() {
+		// enable local DNS
+		System.clearProperty("sun.net.spi.nameservice.provider.1");
+		System.clearProperty("sun.net.spi.nameservice.provider.2");
+
+		// enable DNS cache.
+		Security.setProperty("networkaddress.cache.ttl", "1");
+	}
+
+	@Test
+	@Ignore("Only this test is available when we provide JVM options on test runner")
+	public void testCustomDNSLookup() throws UnknownHostException {
+		System.out.println("Test with custom nameservice provider.");
+		beforeCustomDNS();
+		performLookup("www.google.com");
+		performLookup("www.google2.com");
+		performLookup("www.baidu.com");
+		performLookup("www.naver.com");
+		
+		NameStore.getInstance().put("mydomain.com", "10.10.10.10");
+		//performLookupLocal("mydomain.com");
+		performLookup("mydomain.com");
+		cleanCustomDNS();
+	}
+
+	@Test
+	@Ignore("Only this test is available when we provide JVM options on test runner")
+	public void testOriginalDNSLookup() throws UnknownHostException {
+		System.out.println("Clear nameservice provider from system properties and test.");
+		cleanCustomDNS();
+		performLookup("www.google.com");
+		performLookup("www.google2.com");
+		performLookup("www.baidu.com");
+		performLookup("www.naver.com");
+		
+		NameStore.getInstance().put("mydomain.com", "10.10.10.10");
+		//performLookupLocal("mydomain.com");
+		performLookup("mydomain.com");
 
 	}
 
 	@Test
-	@Ignore("Only this test is avalible when we provide JVM options on test runner")
-	public void testCustomDNSLookup() throws UnknownHostException {
-		performLookup("www.google.com");
-		performLookup("www.google2.com");
+	public void testLocalDNSLookup() throws UnknownHostException {
+		System.out.println("test LocalDNSLookup.....");
+		cleanCustomDNS();
+		performLookupLocal("www.google.com");
+		performLookupLocal("www.google2.com");
+		performLookup("www.baidu.com");
+		performLookup("www.naver.com");
+		
+		NameStore.getInstance().put("mydomain.com", "10.10.10.10");
+		//performLookupLocal("mydomain.com");
+		performLookupLocal("mydomain.com");
 
 	}
 
 	public static void performLookup(String hostName) throws UnknownHostException {
 		InetAddress[] addrs = InetAddress.getAllByName(hostName);
+		for (int j = 0; j < addrs.length; j++) {
+			System.out.println(addrs[j].toString());
+		}
+	}
+
+	public void performLookupLocal(String hostName) throws UnknownHostException {
+		InetAddress[] addrs = localNS.lookupAllHostAddr(hostName);
 		for (int j = 0; j < addrs.length; j++) {
 			System.out.println(addrs[j].toString());
 		}
