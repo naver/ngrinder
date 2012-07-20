b17f94c0dabed7b45056d5aa73d7f141965b1cf7
Tobi <lzfstory@gmail.com>
2012-07-20 14:23:16 +0800

    [NGRINDER-52] develop test report page, including server side and UI
    monitor in agent/target server.

---
+++ ngrinder-core/src/test/java/org/ngrinder/monitor/controller/MonitorRecoderDemo.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2012 - 2012 NHN Corporation
+ * All rights reserved.
+ *
+ * This file is part of The nGrinder software distribution. Refer to
+ * the file LICENSE which is part of The nGrinder distribution for
+ * licensing details. The nGrinder distribution is available on the
+ * Internet at http://nhnopensource.org/ngrinder
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.ngrinder.monitor.controller;
+
+import org.ngrinder.monitor.controller.domain.MonitorAgentInfo;
+import org.ngrinder.monitor.controller.domain.MonitorRecoder;
+import org.ngrinder.monitor.share.domain.JavaInfo;
+import org.ngrinder.monitor.share.domain.SystemInfo;
+
+/**
+ * Class description.
+ * 
+ * @author Tobi
+ * @since
+ * @date 2012-7-20
+ */
+public class MonitorRecoderDemo implements MonitorRecoder {
+
+	@Override
+	public void before() {
+		System.out.println("before");
+
+	}
+
+	@Override
+	public void recoderJavaInfo(String key, JavaInfo javaInfo, MonitorAgentInfo agentInfo) {
+		System.out.println(key);
+		System.out.println(javaInfo);
+	}
+
+	@Override
+	public void recoderSystemInfo(String key, SystemInfo systemInfo, MonitorAgentInfo agentInfo) {
+		System.out.println(key);
+		System.out.println(systemInfo);
+	}
+
+	@Override
+	public void after() {
+		System.out.println("after");
+	}
+
+}
