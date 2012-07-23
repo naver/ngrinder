bf40e68820c62f45788cbbf77572af637ac1c1d8
Tobi <lzfstory@gmail.com>
2012-07-23 14:16:59 +0800

    [NGRINDER-52] develop test report page, including server side and UI

--- ngrinder-controller/src/main/java/org/ngrinder/agent/model/Agent.java
+++ ngrinder-controller/src/main/java/org/ngrinder/agent/model/Agent.java
@@ -99,4 +99,38 @@ public class Agent extends BaseEntity<Agent> {
 		this.region = region;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + appPort;
+		result = prime * result + ((ip == null) ? 0 : ip.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Agent other = (Agent) obj;
+		if (appPort != other.appPort) {
+			return false;
+		}
+		if (ip == null) {
+			if (other.ip != null) {
+				return false;
+			}
+		} else if (!ip.equals(other.ip)) {
+			return false;
+		}
+		return true;
+	}
+
 }
