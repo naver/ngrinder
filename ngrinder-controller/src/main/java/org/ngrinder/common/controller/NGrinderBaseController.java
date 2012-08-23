13cb86bd3cb425074baa759af5bc710eee77516a
Mavlarn <mavlarn@gmail.com>
2012-08-23 17:49:51 +0800

    [NGRINDER-148]  Resolve locale related problem
    Set timezone in within freemarker, then the server time can be converted
    into user time.
    For the parameter got from user request, just make sure that we send
    Date type to server.

--- ngrinder-controller/src/main/java/org/ngrinder/common/controller/NGrinderBaseController.java
+++ ngrinder-controller/src/main/java/org/ngrinder/common/controller/NGrinderBaseController.java
@@ -22,14 +22,11 @@
  */
 package org.ngrinder.common.controller;
 
-import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
 import org.ngrinder.common.constant.NGrinderConstants;
 import org.ngrinder.model.User;
-import org.ngrinder.perftest.model.PerfTest;
 import org.ngrinder.user.service.UserContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -68,81 +65,12 @@ public class NGrinderBaseController implements NGrinderConstants {
 		//userService.saveUser(user);
 	}
 
-	//not we use '@ModelAttribute("currentUser")' to add currentUser
-//	protected void setCurrentUserInfoForModel(ModelMap model) {
-//		model.put(PARAM_USERID, getCurrentUser().getUserId());
-//		model.put(PARAM_TIMEZONE, getCurrentUser().getTimeZone());
-//	}
-
 	protected int getOffSet(String userLocalId) {
 		if (userLocalId == null) {
 			return 0;
 		}
 		return TimeZone.getDefault().getRawOffset() - TimeZone.getTimeZone(userLocalId).getRawOffset();
 	}
-
-	protected void convertServerTimeToUserTime(List<?> list, String userLocalId) {
-		int rawOffset = getOffSet(userLocalId);
-
-		for (Object obj : list) {
-			if (obj instanceof PerfTest) {
-				PerfTest pro = (PerfTest) obj;
-				modifyPerfTestDate(rawOffset, pro);
-			}
-		}
-	}
-
-	/**
-	 * Modify the date in PerfTest object with the provided locale
-	 * @param rawOffset
-	 * @param pro
-	 */
-	private void modifyPerfTestDate(int rawOffset, PerfTest pro) {
-		Date lastModified = pro.getLastModifiedDate();
-		if (lastModified != null) {
-			pro.setLastModifiedDate(new Date(lastModified.getTime() - rawOffset));
-		}
-		Date createdDate = pro.getCreatedDate();
-		if (createdDate != null) {
-			pro.setCreatedDate(new Date(createdDate.getTime() - rawOffset));
-		}
-		Date scheduledTime = pro.getScheduledTime();
-		if (lastModified != null) {
-			pro.setScheduledTime(new Date(scheduledTime.getTime() - rawOffset));
-		}
-	}
-
-	protected void convertServerTimeToUserTimeForBean(Object obj, String userLocalId) {
-		int rawOffset = getOffSet(userLocalId);
-		if (obj instanceof PerfTest) {
-			PerfTest pro = (PerfTest) obj;
-			modifyPerfTestDate(rawOffset, pro);
-		}
-	}
-
-	//it should not be used any more.
-//	protected void convertPropertiesKeys(GrinderProperties props) {
-//		Set<Object> copyKeySet = new HashSet<Object>(props.keySet());
-//
-//		for (Object obj : copyKeySet) {
-//			String key = (String) obj;
-//
-//			if (key.startsWith("grinder.") || key.startsWith("ngrinder.")) {
-//				Object value = props.get(key);
-//				props.remove(key);
-//				props.put(key.replace(".", "_"), value);
-//			}
-//		}
-//	}
-//
-//	protected Map<String, Object> getMessageMap(Object isSuccess, String message) {
-//		Map<String, Object> result = new HashMap<String, Object>();
-//
-//		result.put(JSON_SUCCESS, isSuccess);
-//		result.put(JSON_MESSAGE, message);
-//
-//		return result;
-//	}
 
 	protected String getErrorMessages(String key) {
 		Locale locale = null;
