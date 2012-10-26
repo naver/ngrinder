fd28a29a5fcbc693e5ad92572e7060e5235bd7b9
JunHo Yoon <junoyoon@nhn.com>
2012-10-26 16:27:17 +0900

    [NGRINDER-152] Make spring security more stable - Block read access

--- ngrinder-dns/src/main/java/org/ngrinder/sm/NGrinderSecurityManager.java
+++ ngrinder-dns/src/main/java/org/ngrinder/sm/NGrinderSecurityManager.java
@@ -24,10 +24,13 @@ package org.ngrinder.sm;
 
 import java.io.File;
 import java.io.FileDescriptor;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.security.Permission;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.io.FilenameUtils;
 
 /**
  * nGrinder security manager.
@@ -79,7 +82,6 @@ public class NGrinderSecurityManager extends SecurityManager {
 			je = new File(je).getAbsolutePath();
 			readAllowedDirectory.add(je);
 		}
-
 		writeAllowedDirectory.add(workDirectory);
 		writeAllowedDirectory.add(logDirectory);
 
@@ -157,6 +159,15 @@ public class NGrinderSecurityManager extends SecurityManager {
 	@Override
 	public void checkWrite(String file) {
 		this.fileAccessWriteAllowed(file);
+
+	}
+
+	public String getCanonicalPath(String file) {
+		try {
+			return new File(file).getCanonicalPath();
+		} catch (IOException e) {
+			return null;
+		}
 	}
 
 	@Override
@@ -177,7 +188,14 @@ public class NGrinderSecurityManager extends SecurityManager {
 	 *            file path
 	 */
 	private void fileAccessReadAllowed(String file) {
-		// Alllow all
+		String filePath = normalize(file, workDirectory);
+		for (String dir : readAllowedDirectory) {
+			if (filePath.startsWith(dir)) {
+				return;
+			}
+		}
+
+		throw new SecurityException("Read write access on " + file + "(" + filePath + ") is not allowed.");
 	}
 
 	/**
@@ -188,13 +206,13 @@ public class NGrinderSecurityManager extends SecurityManager {
 	 *            file path
 	 */
 	private void fileAccessWriteAllowed(String file) {
-		String filePath = new File(file).getAbsolutePath();
+		String filePath = normalize(file, workDirectory);
 		for (String dir : writeAllowedDirectory) {
 			if (filePath.startsWith(dir)) {
 				return;
 			}
 		}
-		throw new SecurityException("File write access on " + file + " is not allowed.");
+		throw new SecurityException("File write access on " + file + "(" + filePath + ") is not allowed.");
 	}
 
 	/**
@@ -205,13 +223,13 @@ public class NGrinderSecurityManager extends SecurityManager {
 	 *            file path
 	 */
 	private void fileAccessDeleteAllowed(String file) {
-		String filePath = new File(file).getAbsolutePath();
+		String filePath = normalize(file, workDirectory);
 		for (String dir : deleteAllowedDirectory) {
 			if (filePath.startsWith(dir)) {
 				return;
 			}
 		}
-		throw new SecurityException("File delete access on " + file + " is not allowed.");
+		throw new SecurityException("File delete access on " + file + "(" + filePath + ") is not allowed.");
 	}
 
 	@Override
@@ -229,6 +247,13 @@ public class NGrinderSecurityManager extends SecurityManager {
 		this.netWorkAccessAllowed(host);
 	}
 
+	public String normalize(String filename, String workingDirectory) {
+		if (getPrefixLength(filename) == 0) {
+			filename = workingDirectory + File.separator + filename;
+		}
+		return doNormalize(filename, SYSTEM_SEPARATOR, true);
+	}
+
 	/**
 	 * NetWork access is allowed on "ngrinder.etc.hosts".
 	 * 
@@ -239,7 +264,246 @@ public class NGrinderSecurityManager extends SecurityManager {
 		if (allowedHost.contains(host)) {
 			return;
 		}
-		throw new SecurityException("NetWork access on " + host + " is not allowed. Please add " + host + " on the target host setting.");
+		throw new SecurityException("NetWork access on " + host + " is not allowed. Please add " + host
+						+ " on the target host setting.");
+	}
+
+	/**
+	 * The system separator character.
+	 */
+	private static final char SYSTEM_SEPARATOR = File.separatorChar;
+
+	/**
+	 * The Unix separator character.
+	 */
+	private static final char UNIX_SEPARATOR = '/';
+
+	/**
+	 * The Windows separator character.
+	 */
+	private static final char WINDOWS_SEPARATOR = '\\';
+
+	/**
+	 * The separator character that is the opposite of the system separator.
+	 */
+	private static final char OTHER_SEPARATOR;
+	static {
+		if (isSystemWindows()) {
+			OTHER_SEPARATOR = UNIX_SEPARATOR;
+		} else {
+			OTHER_SEPARATOR = WINDOWS_SEPARATOR;
+		}
+	}
+
+	// -----------------------------------------------------------------------
+	/**
+	 * Determines if Windows file system is in use.
+	 * 
+	 * @return true if the system is Windows
+	 */
+	static boolean isSystemWindows() {
+		return SYSTEM_SEPARATOR == WINDOWS_SEPARATOR;
+	}
+
+	/**
+	 * Internal method to perform the normalization.
+	 * 
+	 * @param filename
+	 *            the filename
+	 * @param separator
+	 *            The separator character to use
+	 * @param keepSeparator
+	 *            true to keep the final separator
+	 * @return the normalized filename
+	 */
+	private static String doNormalize(String filename, char separator, boolean keepSeparator) {
+		if (filename == null) {
+			return null;
+		}
+		int size = filename.length();
+		if (size == 0) {
+			return filename;
+		}
+		int prefix = getPrefixLength(filename);
+		if (prefix < 0) {
+			return null;
+		}
+
+		char[] array = new char[size + 2]; // +1 for possible extra slash, +2 for arraycopy
+		filename.getChars(0, filename.length(), array, 0);
+
+		// fix separators throughout
+		char otherSeparator = (separator == SYSTEM_SEPARATOR ? OTHER_SEPARATOR : SYSTEM_SEPARATOR);
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == otherSeparator) {
+				array[i] = separator;
+			}
+		}
+
+		// add extra separator on the end to simplify code below
+		boolean lastIsDirectory = true;
+		if (array[size - 1] != separator) {
+			array[size++] = separator;
+			lastIsDirectory = false;
+		}
+
+		// adjoining slashes
+		for (int i = prefix + 1; i < size; i++) {
+			if (array[i] == separator && array[i - 1] == separator) {
+				System.arraycopy(array, i, array, i - 1, size - i);
+				size--;
+				i--;
+			}
+		}
+
+		// dot slash
+		for (int i = prefix + 1; i < size; i++) {
+			if (array[i] == separator && array[i - 1] == '.' && (i == prefix + 1 || array[i - 2] == separator)) {
+				if (i == size - 1) {
+					lastIsDirectory = true;
+				}
+				System.arraycopy(array, i + 1, array, i - 1, size - i);
+				size -= 2;
+				i--;
+			}
+		}
+
+		// double dot slash
+		outer: for (int i = prefix + 2; i < size; i++) {
+			if (array[i] == separator && array[i - 1] == '.' && array[i - 2] == '.'
+							&& (i == prefix + 2 || array[i - 3] == separator)) {
+				if (i == prefix + 2) {
+					return null;
+				}
+				if (i == size - 1) {
+					lastIsDirectory = true;
+				}
+				int j;
+				for (j = i - 4; j >= prefix; j--) {
+					if (array[j] == separator) {
+						// remove b/../ from a/b/../c
+						System.arraycopy(array, i + 1, array, j + 1, size - i);
+						size -= (i - j);
+						i = j + 1;
+						continue outer;
+					}
+				}
+				// remove a/../ from a/../c
+				System.arraycopy(array, i + 1, array, prefix, size - i);
+				size -= (i + 1 - prefix);
+				i = prefix + 1;
+			}
+		}
+
+		if (size <= 0) { // should never be less than 0
+			return "";
+		}
+		if (size <= prefix) { // should never be less than prefix
+			return new String(array, 0, size);
+		}
+		if (lastIsDirectory && keepSeparator) {
+			return new String(array, 0, size); // keep trailing separator
+		}
+		return new String(array, 0, size - 1); // lose trailing separator
+	}
+
+	// -----------------------------------------------------------------------
+	/**
+	 * Returns the length of the filename prefix, such as <code>C:/</code> or <code>~/</code>.
+	 * <p>
+	 * This method will handle a file in either Unix or Windows format.
+	 * <p>
+	 * The prefix length includes the first slash in the full filename if applicable. Thus, it is
+	 * possible that the length returned is greater than the length of the input string.
+	 * 
+	 * <pre>
+	 * Windows:
+	 * a\b\c.txt           --> ""          --> relative
+	 * \a\b\c.txt          --> "\"         --> current drive absolute
+	 * C:a\b\c.txt         --> "C:"        --> drive relative
+	 * C:\a\b\c.txt        --> "C:\"       --> absolute
+	 * \\server\a\b\c.txt  --> "\\server\" --> UNC
+	 * 
+	 * Unix:
+	 * a/b/c.txt           --> ""          --> relative
+	 * /a/b/c.txt          --> "/"         --> absolute
+	 * ~/a/b/c.txt         --> "~/"        --> current user
+	 * ~                   --> "~/"        --> current user (slash added)
+	 * ~user/a/b/c.txt     --> "~user/"    --> named user
+	 * ~user               --> "~user/"    --> named user (slash added)
+	 * </pre>
+	 * <p>
+	 * The output will be the same irrespective of the machine that the code is running on. ie. both
+	 * Unix and Windows prefixes are matched regardless.
+	 * 
+	 * @param filename
+	 *            the filename to find the prefix in, null returns -1
+	 * @return the length of the prefix, -1 if invalid or null
+	 */
+	public static int getPrefixLength(String filename) {
+		if (filename == null) {
+			return -1;
+		}
+		int len = filename.length();
+		if (len == 0) {
+			return 0;
+		}
+		char ch0 = filename.charAt(0);
+		if (ch0 == ':') {
+			return -1;
+		}
+		if (len == 1) {
+			if (ch0 == '~') {
+				return 2; // return a length greater than the input
+			}
+			return (isSeparator(ch0) ? 1 : 0);
+		} else {
+			if (ch0 == '~') {
+				int posUnix = filename.indexOf(UNIX_SEPARATOR, 1);
+				int posWin = filename.indexOf(WINDOWS_SEPARATOR, 1);
+				if (posUnix == -1 && posWin == -1) {
+					return len + 1; // return a length greater than the input
+				}
+				posUnix = (posUnix == -1 ? posWin : posUnix);
+				posWin = (posWin == -1 ? posUnix : posWin);
+				return Math.min(posUnix, posWin) + 1;
+			}
+			char ch1 = filename.charAt(1);
+			if (ch1 == ':') {
+				ch0 = Character.toUpperCase(ch0);
+				if (ch0 >= 'A' && ch0 <= 'Z') {
+					if (len == 2 || isSeparator(filename.charAt(2)) == false) {
+						return 2;
+					}
+					return 3;
+				}
+				return -1;
+
+			} else if (isSeparator(ch0) && isSeparator(ch1)) {
+				int posUnix = filename.indexOf(UNIX_SEPARATOR, 2);
+				int posWin = filename.indexOf(WINDOWS_SEPARATOR, 2);
+				if ((posUnix == -1 && posWin == -1) || posUnix == 2 || posWin == 2) {
+					return -1;
+				}
+				posUnix = (posUnix == -1 ? posWin : posUnix);
+				posWin = (posWin == -1 ? posUnix : posWin);
+				return Math.min(posUnix, posWin) + 1;
+			} else {
+				return (isSeparator(ch0) ? 1 : 0);
+			}
+		}
+	}
+
+	// -----------------------------------------------------------------------
+	/**
+	 * Checks if the character is a separator.
+	 * 
+	 * @param ch
+	 *            the character to check
+	 * @return true if it is a separator character
+	 */
+	private static boolean isSeparator(char ch) {
+		return (ch == UNIX_SEPARATOR) || (ch == WINDOWS_SEPARATOR);
 	}
 
 }
