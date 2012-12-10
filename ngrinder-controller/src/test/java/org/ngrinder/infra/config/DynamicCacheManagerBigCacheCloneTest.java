ab68defc4ad92e9d12dbab4c3a7530cd978a2abe
JunHo Yoon <junoyoon@nhn.com>
2012-12-10 14:23:41 +0900

    [NOBTS] Add cache test

--- ngrinder-controller/src/test/java/org/ngrinder/infra/config/DynamicCacheManagerBigCacheCloneTest.java
+++ ngrinder-controller/src/test/java/org/ngrinder/infra/config/DynamicCacheManagerBigCacheCloneTest.java
@@ -1,13 +1,12 @@
 package org.ngrinder.infra.config;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import net.grinder.util.NetworkUtil;
 import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Ehcache;
 
 import org.junit.After;
 import org.junit.Before;
@@ -16,7 +15,7 @@ import org.ngrinder.common.util.ThreadUtil;
 import org.springframework.cache.Cache;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 
-public class DynamicCacheManagerTest {
+public class DynamicCacheManagerBigCacheCloneTest {
 	ReloadableDynamicCacheManager dynamicCacheManagerConfig1;
 	ReloadableDynamicCacheManager dynamicCacheManagerConfig2;
 	private EhCacheCacheManager dynamicCacheManager1;
@@ -48,38 +47,38 @@ public class DynamicCacheManagerTest {
 		}
 	}
 
+	Config config;
+	String currentIp;
+
 	@Before
 	public void before() {
+
+	}
+
+	@Test
+	public void testDynamicCache() throws InterruptedException {
 		dynamicCacheManagerConfig1 = new ReloadableDynamicCacheManager(40001, "cache1");
-		Config config = mock(Config.class);
+		config = mock(Config.class);
 		dynamicCacheManagerConfig1.config = config;
-		String currentIp = NetworkUtil.getLocalHostAddress();
+		currentIp = NetworkUtil.getLocalHostAddress();
 		when(config.getClusterURIs()).thenReturn(new String[] { currentIp + ":40001", currentIp + ":40002" });
 		when(config.getCurrentIP()).thenReturn(currentIp);
 		when(config.isCluster()).thenReturn(true);
 		dynamicCacheManager1 = dynamicCacheManagerConfig1.dynamicCacheManager();
 		dynamicCacheManager1.afterPropertiesSet();
+		Cache cache = dynamicCacheManager1.getCache("region_list");
+		for (int i = 0; i < 1000; i++) {
+			cache.put("Hello" + i, i);
+		}
+		ThreadUtil.sleep(3000);
 		dynamicCacheManagerConfig2 = new ReloadableDynamicCacheManager(40002, "cache2");
 		when(config.getClusterURIs()).thenReturn(new String[] { currentIp + ":40001", currentIp + ":40002" });
 		dynamicCacheManagerConfig2.config = config;
 		dynamicCacheManager2 = dynamicCacheManagerConfig2.dynamicCacheManager();
 		dynamicCacheManager2.afterPropertiesSet();
-	}
-
-	@Test
-	public void testDynamicCache() throws InterruptedException {
-		System.setProperty("net.sf.ehcache.skipUpdateCheck", "false");
-		dynamicCacheManager2.getCache("region_list").put("hello", "127.0.0.1");
-		ThreadUtil.sleep(1000);
-		Cache cache = dynamicCacheManager1.getCache("region_list");
-		assertThat((String) cache.get("hello").get(), is("127.0.0.1"));
-		ThreadUtil.sleep(4000);
-		assertThat(dynamicCacheManager1.getCache("region_list").get("hello"), not(nullValue()));
-		assertThat(dynamicCacheManager2.getCache("region_list").get("hello"), not(nullValue()));
-		ThreadUtil.sleep(4000);
-		assertThat(dynamicCacheManager1.getCache("region_list").get("hello"), nullValue());
-		assertThat(dynamicCacheManager2.getCache("region_list").get("hello"), nullValue());
-
+		ThreadUtil.sleep(3000);
+		assertThat(((Ehcache) (dynamicCacheManager2.getCache("region_list").getNativeCache())).getKeys().size(),
+						is(1000));
 	}
 
 	@After
