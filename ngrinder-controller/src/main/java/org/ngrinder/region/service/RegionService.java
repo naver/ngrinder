03517985d556c5769a8d9ae36bead1dfb913fb24
JunHo Yoon <junoyoon@nhn.com>
2012-12-13 22:25:22 +0900

    [NOBTS] Make the region timeout longer

--- ngrinder-controller/src/main/java/org/ngrinder/region/service/RegionService.java
+++ ngrinder-controller/src/main/java/org/ngrinder/region/service/RegionService.java
@@ -41,7 +41,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * Region service class. This class responsible to keep the status of available regions.
+ * Region service class. This class responsible to keep the status of available
+ * regions.
  * 
  * @author Mavlarn
  * @author JunHo Yoon
@@ -59,7 +60,8 @@ public class RegionService {
 	private ScheduledTask scheduledTask;
 
 	/**
-	 * Set current region into cache, using the IP as key and region name as value.
+	 * Set current region into cache, using the IP as key and region name as
+	 * value.
 	 * 
 	 */
 	@PostConstruct
@@ -88,7 +90,8 @@ public class RegionService {
 	}
 
 	/**
-	 * Destroy method. this method is responsible to delete our current region from dist cache.
+	 * Destroy method. this method is responsible to delete our current region
+	 * from dist cache.
 	 */
 	@PreDestroy
 	public void destroy() {
@@ -107,7 +110,7 @@ public class RegionService {
 		List<String> regions = new ArrayList<String>();
 		if (config.isCluster()) {
 			for (File each : config.getHome().getControllerShareDirectory().listFiles()) {
-				if (System.currentTimeMillis() - (1000 * 3) < each.lastModified()) {
+				if (System.currentTimeMillis() - (1000 * 60) < each.lastModified()) {
 					regions.add(each.getName());
 				}
 			}
