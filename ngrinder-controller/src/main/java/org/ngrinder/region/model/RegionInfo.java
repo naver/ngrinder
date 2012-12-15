3352e40e368ebda90138c02d49dea15699214baa
JunHo Yoon <junoyoon@nhn.com>
2012-12-16 03:57:54 +0900

    [NOBTS] Add region info

---
+++ ngrinder-controller/src/main/java/org/ngrinder/region/model/RegionInfo.java
@@ -0,0 +1,82 @@
+/* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package org.ngrinder.region.model;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import net.grinder.common.processidentity.AgentIdentity;
+
+/**
+ * Region info to be shared.
+ * 
+ * @author JunHo Yoon
+ * @since 3.1
+ */
+public class RegionInfo implements Serializable {
+	/**
+	 * UUID
+	 */
+	private static final long serialVersionUID = 1L;
+	private String ip;
+	private boolean visible = true;
+	private Set<AgentIdentity> agentIdentities;
+
+	/**
+	 * @param ip
+	 * @param agentIdentitySet
+	 */
+	public RegionInfo(String ip, Set<AgentIdentity> agentIdentities) {
+		this(ip, agentIdentities, true);
+	}
+
+	/**
+	 * @param ip
+	 * @param agentIdentity
+	 * @param visible
+	 */
+	public RegionInfo(String ip, Set<AgentIdentity> agentIdentities, boolean visible) {
+		super();
+		this.ip = ip;
+		this.visible = visible;
+		this.agentIdentities = agentIdentities;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+
+
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
+	public Set<AgentIdentity> getAgentIdentities() {
+		return agentIdentities;
+	}
+
+	public void setAgentIdentities(Set<AgentIdentity> agentIdentities) {
+		this.agentIdentities = agentIdentities;
+	}
+
+}
