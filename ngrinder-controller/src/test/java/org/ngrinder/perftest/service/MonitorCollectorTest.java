7f96596ae9ab431657efa6144f99b7743fcc53fc
JunHo Yoon <junoyoon@nhn.com>
2013-02-06 17:36:32 +0900

    [NGRINDER-515] Delete monitor package

---
+++ ngrinder-controller/src/test/java/org/ngrinder/perftest/service/MonitorCollectorTest.java
@@ -0,0 +1,35 @@
+package org.ngrinder.perftest.service;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.ngrinder.monitor.controller.model.SystemDataModel;
+import org.python.google.common.collect.Lists;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+public class MonitorCollectorTest {
+
+	@Test
+	public void testMonitor() {
+		SystemDataModel systemDataModel = new SystemDataModel();
+		systemDataModel.setCollectTime(100);
+		systemDataModel.setFreeMemory(20);
+		systemDataModel.setCpuUsedPercentage(20);
+		List<SystemDataModel> lists = Lists.newArrayList();
+		lists.add(systemDataModel);
+		lists.add(systemDataModel);
+		Gson gson = new Gson();
+		String json = gson.toJson(lists);
+		ArrayList<SystemDataModel> fromJson = gson.fromJson(json, getTypeToken());
+		System.out.println(fromJson.get(0).getClass());
+	}
+
+	private Type getTypeToken() {
+		return new TypeToken<ArrayList<SystemDataModel>>() {
+		}.getType();
+	}
+}
---
+++ ngrinder-controller/src/test/java/org/ngrinder/perftest/service/monitor/MontorClientManagerTest.java
@@ -0,0 +1,80 @@
+/* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package org.ngrinder.perftest.service.monitor;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.ngrinder.agent.model.AgentInfo;
+import org.ngrinder.common.util.ThreadUtil;
+import org.ngrinder.monitor.MonitorConstants;
+import org.ngrinder.perftest.service.AbstractPerfTestTransactionalTest;
+import org.ngrinder.perftest.service.monitor.MonitorTask;
+
+/**
+ * Class description.
+ *
+ * @author Mavlarn
+ * @since
+ */
+public class MontorClientManagerTest extends AbstractPerfTestTransactionalTest {
+
+	@Test
+	public void testAddMonitor() throws IOException {
+		File tempRepo = new File(System.getProperty("java.io.tmpdir"), "test-repo");
+		tempRepo.mkdir();
+		tempRepo.deleteOnExit();
+		AgentInfo monitorAgt = new AgentInfo();
+		monitorAgt.setIp("127.0.0.1");
+		monitorAgt.setPort(MonitorConstants.DEFAULT_MONITOR_PORT);
+		Set<AgentInfo> agents = new HashSet<AgentInfo>(2);
+		agents.add(monitorAgt);
+		MonitorTask monitorMngr = applicationContext.getBean(MonitorTask.class);
+		monitorMngr.add(agents, tempRepo);
+		new Thread(monitorMngr).start();
+		monitorMngr.saveData();
+		
+		ThreadUtil.sleep(3000);
+		// test to add again
+		monitorMngr.add(agents, tempRepo);
+
+		ThreadUtil.sleep(3000);
+		monitorMngr.destroy();
+	}
+
+	@Test
+	public void testAddMonitorInvalid() throws IOException {
+		File tempRepo = new File(System.getProperty("java.io.tmpdir"), "test-repo");
+		tempRepo.mkdir();
+		tempRepo.deleteOnExit();
+		AgentInfo monitorAgt = new AgentInfo();
+		monitorAgt.setIp("10.10.10.10");
+		monitorAgt.setPort(MonitorConstants.DEFAULT_MONITOR_PORT);
+		AgentInfo monitorAgt2 = new AgentInfo();
+		monitorAgt2.setIp("localhost");
+		monitorAgt2.setPort(MonitorConstants.DEFAULT_MONITOR_PORT);
+		Set<AgentInfo> agents = new HashSet<AgentInfo>(2);
+		agents.add(monitorAgt);
+		agents.add(monitorAgt2);
+		MonitorTask monitorMngr = applicationContext.getBean(MonitorTask.class);
+		monitorMngr.add(agents, tempRepo);
+		new Thread(monitorMngr).start();
+		monitorMngr.saveData();
+		ThreadUtil.sleep(3000);
+		monitorMngr.destroy();
+	}
+}
