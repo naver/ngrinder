ccf97397d42ccbfd149da764091bc6ff1590e4d1
JunHo Yoon <junoyoon@nhn.com>
2013-05-02 23:09:10 +0900

    [NGRINDER-594] Add jython syntaxcheck test

---
+++ ngrinder-controller/src/test/java/org/ngrinder/script/handler/JythonHandlerTest.java
@@ -0,0 +1,18 @@
+package org.ngrinder.script.handler;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class JythonHandlerTest {
+	@Test
+	public void testJythonSyntaxError() {
+		JythonScriptHandler handler = new JythonScriptHandler();
+		String checkSyntaxErrors = handler.checkSyntaxErrors("hello", "print (( 'hello';");
+		assertThat(checkSyntaxErrors, notNullValue());
+		checkSyntaxErrors = handler.checkSyntaxErrors("hello", "print 'hello';");
+		assertThat(checkSyntaxErrors, nullValue());
+	}
+}
