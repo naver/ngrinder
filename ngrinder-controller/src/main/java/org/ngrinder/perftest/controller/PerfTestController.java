3126eefc011f96ef3fb5420833ec9fca8bb344d9
junoyoon <junoyoon@nhn.com>
2013-11-06 13:34:02 +0900

    [NGRINDER-684] Fix typo errors

--- ngrinder-controller/src/main/java/org/ngrinder/perftest/controller/PerfTestController.java
+++ ngrinder-controller/src/main/java/org/ngrinder/perftest/controller/PerfTestController.java
@@ -873,12 +873,12 @@ public class PerfTestController extends NGrinderBaseController {
 			if (memUsage > 99.9f) {
 				memUsage = 99.9f;
 			}
-			String perfTString = String.format(" {'agent' : '%s', 'agentFull' : '%s', 'cpu' : '%s',"
+			String perfString = String.format(" {'agent' : '%s', 'agentFull' : '%s', 'cpu' : '%s',"
 					+ " 'mem' : '%s', 'sentPerSec' : '%s', 'recievedPerSec' : '%s'}",
 					StringUtils.abbreviate(each.getKey(), 15), each.getKey(), format.format(cpuUsedPercentage),
 					format.format(memUsage), UnitUtil.byteCountToDisplaySize(sentPerSec),
 					UnitUtil.byteCountToDisplaySize(recievedPerSec));
-			perfStringList.add(perfTString);
+			perfStringList.add(perfString);
 		}
 		return StringUtils.join(perfStringList, ",");
 	}
