49f4b234c160b186fa5c6afd9c1d7e8d5fec90e8
junoyoon <junoyoon@nhn.com>
2013-12-11 01:25:31 +0900

    [NOBTS] Add the message refresh for dev

---
+++ ngrinder-controller/src/main/java/org/ngrinder/operation/cotroller/DevController.java
@@ -0,0 +1,57 @@
+/* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package org.ngrinder.operation.cotroller;
+
+import org.ngrinder.common.controller.BaseController;
+import org.ngrinder.common.controller.RestAPI;
+import org.ngrinder.operation.service.SystemConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.HttpEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.ngrinder.common.util.Preconditions.checkNotEmpty;
+
+/**
+ * Developer Feature.
+ *
+ * @since 3.3
+ */
+@Controller
+@RequestMapping("/dev")
+public class DevController extends BaseController {
+
+	@Autowired
+	private ReloadableResourceBundleMessageSource messageSource;
+
+	/**
+	 * Refresh the messages.
+	 *
+	 * @param model model
+	 * @return "redirect:/"
+	 */
+	@RequestMapping("/msg")
+	public String refreshMessage(ModelMap model) {
+		messageSource.clearCacheIncludingAncestors();
+		model.clear();
+		return "redirect:/";
+	}
+
+}
