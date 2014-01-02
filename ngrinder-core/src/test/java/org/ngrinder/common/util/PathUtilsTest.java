81c3188b825fb677b36ccdad3b2614b5a4a2d4ca
junoyoon <junoyoon@nhn.com>
2014-01-02 16:03:23 +0900

    Fix a bug

--- ngrinder-core/src/test/java/org/ngrinder/common/util/PathUtilsTest.java
+++ ngrinder-core/src/test/java/org/ngrinder/common/util/PathUtilsTest.java
@@ -8,13 +8,13 @@ import static org.fest.assertions.Assertions.assertThat;
 public class PathUtilsTest {
 	@Test
 	public void testJoin() throws Exception {
-		assertThat(PathUtils.join("/hello/world", "wow")).isEqualTo("/hello/world/wow");
-		assertThat(PathUtils.join("/hello/world", "wow/")).isEqualTo("/hello/world/wow");
-		assertThat(PathUtils.join("/hello/world//", "wow/")).isEqualTo("/hello/world/wow");
-		assertThat(PathUtils.join("/hello/world////", "//wow/")).isEqualTo("/hello/world/wow");
-		assertThat(PathUtils.join("/////hello/world////", "//wow/")).isEqualTo("/hello/world/wow");
-		assertThat(PathUtils.join("hello/world////", "//wow/")).isEqualTo("/hello/world/wow");
-		assertThat(PathUtils.join("hello/world////", "//wow/")).isEqualTo("/hello/world/wow");
-		assertThat(PathUtils.join("/hello/world////", "//wow////")).isEqualTo("/hello/world/wow");
+		assertThat(PathUtils.join("/hello/world", "wow")).isEqualTo("hello/world/wow");
+		assertThat(PathUtils.join("/hello/world", "wow/")).isEqualTo("hello/world/wow");
+		assertThat(PathUtils.join("/hello/world//", "wow/")).isEqualTo("hello/world/wow");
+		assertThat(PathUtils.join("/hello/world////", "//wow/")).isEqualTo("hello/world/wow");
+		assertThat(PathUtils.join("/////hello/world////", "//wow/")).isEqualTo("hello/world/wow");
+		assertThat(PathUtils.join("hello/world////", "//wow/")).isEqualTo("hello/world/wow");
+		assertThat(PathUtils.join("hello/world////", "//wow/")).isEqualTo("hello/world/wow");
+		assertThat(PathUtils.join("/hello/world////", "//wow////")).isEqualTo("hello/world/wow");
 	}
 }
