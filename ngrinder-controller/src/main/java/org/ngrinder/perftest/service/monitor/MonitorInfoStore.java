8940f161172ea2da505b8b8be3cf645255cf4fb4
junoyoon <junoyoon@nhn.com>
2014-01-07 13:05:19 +0900

    delete unnecessary code

--- ngrinder-controller/src/main/java/org/ngrinder/perftest/service/monitor/MonitorInfoStore.java
+++ ngrinder-controller/src/main/java/org/ngrinder/perftest/service/monitor/MonitorInfoStore.java
@@ -19,7 +19,6 @@ import org.ngrinder.monitor.share.domain.SystemInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
