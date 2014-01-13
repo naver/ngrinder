002c5018c30964551f0bbc034a4bf8f6cdccc354
junoyoon <junoyoon@nhn.com>
2014-01-13 18:42:31 +0900

    fix stop command

--- ngrinder-sh/src/main/shell/stop_monitor.sh
+++ ngrinder-sh/src/main/shell/stop_monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curpath=`dirname $0`
 cd ${curpath}
-java -server -cp "lib/*" org.ngrinder.NGrinderAgentStarter -mode=monitor -command=stop %*
+java -server -cp "lib/*" org.ngrinder.NGrinderAgentStarter -mode=monitor -command=stop $@
