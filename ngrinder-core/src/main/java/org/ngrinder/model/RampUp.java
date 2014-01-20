7c908a81c5900db87bdbb92503573f41a55c96f9
junoyoon <junoyoon@nhn.com>
2014-01-20 12:09:21 +0900

    [NGRINDER-718] Add javadoc

--- ngrinder-core/src/main/java/org/ngrinder/model/RampUp.java
+++ ngrinder-core/src/main/java/org/ngrinder/model/RampUp.java
@@ -1,7 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ngrinder.model;
 
+/**
+ * RampUp model.
+ *
+ * @since 3.3
+ */
 public enum RampUp {
+	/**
+	 * Process Ramp Up
+	 */
 	PROCESS("process"),
+	/**
+	 * Thread Ramp Up
+	 */
 	THREAD("thread");
 	private String messageKey;
 
@@ -9,6 +33,7 @@ public enum RampUp {
 		this.messageKey = messageKey;
 	}
 
+	@SuppressWarnings("UnusedDeclaration")
 	public String getMessageKey() {
 		return messageKey;
 	}
