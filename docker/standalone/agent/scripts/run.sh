028bdf75d46bf00d77cfc6a683d1690c8314c4a3
nanpa83 <rankyung.hong@gmail.com>
2015-08-13 17:58:03 +0900

    modified agent binary download logic

--- docker/standalone/agent/scripts/run.sh
+++ docker/standalone/agent/scripts/run.sh
@@ -3,27 +3,42 @@
 if [ -n "$CONTROLLER_ADDR" ]
 then
 	AGENT_URL="http://${CONTROLLER_ADDR}/agent/download"
+	AGENT="${BASE_DIR}/ngrinder-agent"
 	CNT=1
 	cd ${BASE_DIR}
-
-	while true
-	do
-		echo "trying to download agent from controller ${AGENT_URL}"
-		wget -O ngrinder-agent.tar ${AGENT_URL}
-		if [ $? -eq 0 ]; then
-			tar -xvf ngrinder-agent.tar
-			if [ -d "${BASE_DIR}/ngrinder-agent" ]; then
-        			${BASE_DIR}/ngrinder-agent/run_agent.sh
-				break
-			else
-				echo "Failed extracting ngrinder-agent.tar file"
+	echo "deleting pid..."
+	rm -rf ~/.ngrinder_agent/pid
+        if [ -d ${AGENT} ]; then
+		echo "agent binary already exists."
+	        ${AGENT}/run_agent.sh
+	else
+		while true
+		do
+			echo "trying to download agent from controller ${AGENT_URL}"
+			if [ -f "${AGENT}.tar" ]; then
+				rm "${AGENT}.tar"
 			fi
-		else
-			echo "Failed downloading agent"
-		fi
+			wget -O ngrinder-agent.tar ${AGENT_URL}
+			if [ $? -eq 0 ]; then
+				tar -xvf ngrinder-agent.tar
+				if [ -d "${AGENT}" ]; then
+        				${AGENT}/run_agent.sh
+					break
+				else
+					echo "Failed extracting ngrinder-agent.tar file"
+				fi
+			else
+				echo "Failed downloading agent"
+			fi
 		
-		sleep 5
-	done
+			sleep 5
+	                CNT=$((CNT + 1))
+	                echo $CNT
+	                if [ $CNT -eq 3 ]; then
+	                        break
+	                fi
+		done
+	fi
 else
 	echo "Please set CONTROLLER_ADDR environment variable"
 fi
