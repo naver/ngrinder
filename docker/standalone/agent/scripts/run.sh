370e8d531dfff75bb3cb3221a4579901ae40b75b
swc <swc@mom-master.(none)>
2015-08-13 15:18:17 +0900

    modified agen file download logic

--- docker/standalone/agent/scripts/run.sh
+++ docker/standalone/agent/scripts/run.sh
@@ -3,27 +3,34 @@
 if [ -n "$CONTROLLER_ADDR" ]
 then
 	AGENT_URL="http://${CONTROLLER_ADDR}/agent/download"
-	CNT=1
+	AGENT="${BASE_DIR}/ngrinder-agent"
 	cd ${BASE_DIR}
 
-	while true
-	do
-		echo "trying to download agent from controller ${AGENT_URL}"
-		wget -O ngrinder-agent.tar ${AGENT_URL}
-		if [ $? -eq 0 ]; then
-			tar -xvf ngrinder-agent.tar
-			if [ -d "${BASE_DIR}/ngrinder-agent" ]; then
-        			${BASE_DIR}/ngrinder-agent/run_agent.sh
-				break
-			else
-				echo "Failed extracting ngrinder-agent.tar file"
+        if [ -d ${AGENT} ]; then
+	        ${AGENT}/run_agent.sh
+	else
+		while true
+		do
+			echo "trying to download agent from controller ${AGENT_URL}"
+			if [ -f "${AGENT}.tar" ]; then
+				rm "${AGENT}.tar"
 			fi
-		else
-			echo "Failed downloading agent"
-		fi
+			wget -O ngrinder-agent.tar ${AGENT_URL}
+			if [ $? -eq 0 ]; then
+				tar -xvf ngrinder-agent.tar
+				if [ -d "${AGENT}" ]; then
+        				${AGENT}/run_agent.sh
+					break
+				else
+					echo "Failed extracting ngrinder-agent.tar file"
+				fi
+			else
+				echo "Failed downloading agent"
+			fi
 		
-		sleep 5
-	done
+			sleep 5
+		done
+	fi
 else
 	echo "Please set CONTROLLER_ADDR environment variable"
 fi
