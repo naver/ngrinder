f820dc6cfd0560a0c0d020192c1d1c5c42497e31
junoyoon <juno.yoon@navercorp.com>
2016-07-02 00:11:53 +0900

    #184 fix circular injection

--- ngrinder-controller/src/main/java/org/ngrinder/infra/plugin/finder/NGrinderDefaultExtensionFinder.java
+++ ngrinder-controller/src/main/java/org/ngrinder/infra/plugin/finder/NGrinderDefaultExtensionFinder.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import ro.fortsoft.pf4j.DefaultExtensionFinder;
+import ro.fortsoft.pf4j.ExtensionFinder;
+import ro.fortsoft.pf4j.ExtensionWrapper;
 import ro.fortsoft.pf4j.PluginManager;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * DefaultExtensionFinder extended class.
@@ -14,18 +19,22 @@ import ro.fortsoft.pf4j.PluginManager;
  * @see https://github.com/decebals/pf4j-spring
  * @since 3.0
  */
-@Component
-public class NGrinderDefaultExtensionFinder extends DefaultExtensionFinder{
+public class NGrinderDefaultExtensionFinder implements ExtensionFinder {
 
-	@Autowired
+	private NGrinderServiceProviderExtensionFinder finder;
+
 	public NGrinderDefaultExtensionFinder(PluginManager pluginManager) {
-		super(pluginManager);
-		finders.add(new NGrinderServiceProviderExtensionFinder(pluginManager));
+		finder = new NGrinderServiceProviderExtensionFinder(pluginManager);
+	}
+
+
+	@Override
+	public <T> List<ExtensionWrapper<T>> find(Class<T> type) {
+		return finder.find(type);
 	}
 
 	@Override
-	protected void addDefaults(PluginManager pluginManager) {
-		// Disable the default ProviderExtensionFinder
+	public Set<String> findClassNames(String pluginId) {
+		return finder.findClassNames(pluginId);
 	}
-
 }
