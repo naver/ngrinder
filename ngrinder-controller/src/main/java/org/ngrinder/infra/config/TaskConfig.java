89f48b66537c42f92075625f1c7e719a42e5d8b6
donggyu-lee <donggyu.lee@nts-corp.com>
2018-12-12 19:50:44 +0900

    Move mutable configurations to the server configuration file

--- ngrinder-controller/src/main/java/org/ngrinder/infra/config/TaskConfig.java
+++ ngrinder-controller/src/main/java/org/ngrinder/infra/config/TaskConfig.java
@@ -2,6 +2,7 @@ package org.ngrinder.infra.config;
 
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
@@ -16,6 +17,12 @@ import java.util.concurrent.Executor;
 @Configuration
 public class TaskConfig implements SchedulingConfigurer, AsyncConfigurer {
 
+	@Value("${server.task.scheduler.pool-size}")
+	private int taskSchedulerPoolSize;
+
+	@Value("${server.task.async-executor.pool-size}")
+	private int asyncExecutorPoolSize;
+
 	@Override
 	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
 		taskRegistrar.setTaskScheduler(taskScheduler());
@@ -24,14 +31,14 @@ public class TaskConfig implements SchedulingConfigurer, AsyncConfigurer {
 	@Bean
 	public TaskScheduler taskScheduler() {
 		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-		taskScheduler.setPoolSize(100);
+		taskScheduler.setPoolSize(taskSchedulerPoolSize);
 		taskScheduler.initialize();
 		return taskScheduler;
 	}
 	@Override
 	public Executor getAsyncExecutor() {
 		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-		taskExecutor.setMaxPoolSize(10);
+		taskExecutor.setMaxPoolSize(asyncExecutorPoolSize);
 		taskExecutor.initialize();
 		return taskExecutor;
 	}
