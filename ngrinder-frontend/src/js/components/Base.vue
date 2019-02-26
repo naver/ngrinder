376b1aa8a353f53818774eb2919be0bdabbb66e5
donggyu-lee <donggyu.lee@nts-corp.com>
2019-02-26 11:20:55 +0900

    Make vue.js base structure

---
+++ ngrinder-frontend/src/js/components/Base.vue
@@ -0,0 +1,27 @@
+<script>
+    import Component from 'vue-class-component';
+    import Vue from 'vue';
+    import { mapState } from 'vuex';
+    import GlobalAttributes from 'global-attributes';
+
+    @Component({
+        computed: {
+            ...mapState([
+                'nGrinderVersion',
+            ]),
+        }
+    })
+    export default class Base extends Vue {
+        mounted() {
+            this.initGlobalAttribtues();
+        }
+
+        initGlobalAttribtues() {
+            this.$store.commit('nGrinderVersion', GlobalAttributes.get('nGrinderVersion'));
+        }
+    }
+</script>
+
+<style lang="less">
+
+</style>
