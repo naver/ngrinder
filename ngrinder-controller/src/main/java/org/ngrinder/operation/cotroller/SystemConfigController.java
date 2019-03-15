19f7c825c0a1a96dd5d807da68418bf91f203eeb
donggyu-lee <donggyu.lee@nts-corp.com>
2019-03-15 12:14:15 +0900

    Use spring default json serialization in SystemConfig

--- ngrinder-controller/src/main/java/org/ngrinder/operation/cotroller/SystemConfigController.java
+++ ngrinder-controller/src/main/java/org/ngrinder/operation/cotroller/SystemConfigController.java
@@ -17,14 +17,13 @@ import org.ngrinder.common.controller.BaseController;
 import org.ngrinder.common.controller.RestAPI;
 import org.ngrinder.operation.service.SystemConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import static org.ngrinder.common.util.Preconditions.checkNotEmpty;
 
@@ -76,9 +75,10 @@ public class SystemConfigController extends BaseController {
 	 * @return system configuration
 	 */
 	@RestAPI
-	@RequestMapping(value = "/api", method = RequestMethod.GET)
-	public HttpEntity<String> getOne() {
-		return toJsonHttpEntity(systemConfigService.getOne());
+	@ResponseBody
+	@RequestMapping("/api")
+	public String getOne() {
+		return systemConfigService.getOne();
 	}
 
 	/**
@@ -88,10 +88,10 @@ public class SystemConfigController extends BaseController {
 	 * @return true if succeeded
 	 */
 	@RestAPI
+	@ResponseBody
 	@RequestMapping(value = "/api", method = RequestMethod.POST)
-	public HttpEntity<String> save(@RequestParam(required = true) final String content) {
-		systemConfigService.save(checkNotEmpty(content, "content should be " +
-				"passed as parameter"));
-		return successJsonHttpEntity();
+	public String save(@RequestParam final String content) {
+		systemConfigService.save(checkNotEmpty(content, "content should be passed as parameter"));
+		return returnSuccess();
 	}
 }
