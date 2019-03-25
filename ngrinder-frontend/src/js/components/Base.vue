adf13197a0b82b2a192c4fd8fa5c3560512d5f55
donggyu-lee <donggyu.lee@nts-corp.com>
2019-03-25 18:54:34 +0900

    Add i18n configuration

--- ngrinder-frontend/src/js/components/Base.vue
+++ ngrinder-frontend/src/js/components/Base.vue
@@ -3,6 +3,7 @@
     import Vue from 'vue';
     import { mapState } from 'vuex';
     import GlobalAttributes from 'global-attributes';
+    import I18n from '../i18n.js';
 
     @Component({
         computed: {
@@ -14,6 +15,10 @@
     export default class Base extends Vue {
         mounted() {
             this.initGlobalAttribtues();
+        }
+
+        i18n(key, args) {
+            return I18n.i18n(key, args);
         }
 
         initGlobalAttribtues() {
---
+++ ngrinder-frontend/src/js/i18n.js
@@ -0,0 +1,54 @@
+import i18next from "i18next";
+import jqueryI18next from "jquery-i18next";
+import Cache from "i18next-localstorage-cache";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+function replaceArgs(msgs) {
+    for (const each in msgs) {
+        const msg = msgs[each];
+        if (msg !== null) {
+            msgs[each] = msg.replace(/\{([0-9])\}/g, "{{arg$1}}");
+        }
+    }
+    return msgs;
+}
+
+const resources = {
+    kr: {
+        translation: require('messages_kr.properties'),
+    },
+    en: {
+        translation: require('messages_en.properties'),
+    },
+    cn: {
+        translation: require('messages_cn.properties'),
+    },
+};
+
+class I18n {
+    constructor() {
+        const options = {
+            keySeparator: "#",
+            detection: {
+                order: ["cookie", "localStorage"],
+                caches: ["localStorage"],
+                lookupCookie: "naveruserlocale",
+                lookupLocalStorage: "naveruserlocale"
+            },
+
+            fallbackLng: "en",
+            resources: resources
+        };
+
+        i18next.use(Cache).use(LanguageDetector).init(options, () => {
+            jqueryI18next.init(i18next, $);
+            $(document.body).localize();
+        });
+    };
+
+    i18n(key, args) {
+        return i18next.t(key, args);
+    }
+}
+
+export default new I18n();
