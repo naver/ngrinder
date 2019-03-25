95037379f6b87735a3815cbd9970e1024a7ac922
donggyu-lee <donggyu.lee@nts-corp.com>
2019-03-25 19:01:32 +0900

    Add Vue object for event bus

---
+++ ngrinder-frontend/src/js/bus-event.js
@@ -0,0 +1,6 @@
+class Event {
+    static get SIGN_UP_FORM_VALIDATION_CHECK() {return 'sign-up-form-validation-check';}
+    static get RESET_SIGN_UP_MODAL() {return 'reset-sign-up-modal';}
+}
+
+export default Event;
