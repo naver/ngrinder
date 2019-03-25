c683d86cc00044a4a6cf6b05f0646d708ec38738
donggyu-lee <donggyu.lee@nts-corp.com>
2019-03-25 19:04:39 +0900

    Add Copyright

---
+++ ngrinder-frontend/src/js/components/common/Copyright.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="foot_div">
+        <div class="credit">
+            <a href="https://naver.github.io/ngrinder" target="_blank" v-text="`nGrinder v${nGrinderVersion}`"></a>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Component from 'vue-class-component';
+
+    @Component({
+        name: 'copyright',
+        props: {
+            nGrinderVersion: {
+                type: String,
+                required: true,
+            },
+        },
+    })
+    export default class Copyright {}
+</script>
+
+<style lang="less" scoped>
+    #foot_div {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+
+        .credit {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    }
+</style>
