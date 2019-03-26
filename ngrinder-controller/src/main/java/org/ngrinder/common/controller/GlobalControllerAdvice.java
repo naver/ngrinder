d4d3083498845dade3ab540d6a26f280a79bbb6c
donggyu-lee <donggyu.lee@nts-corp.com>
2019-03-26 16:26:17 +0900

    Configure global data when vue object initialization

--- ngrinder-controller/src/main/java/org/ngrinder/common/controller/GlobalControllerAdvice.java
+++ ngrinder-controller/src/main/java/org/ngrinder/common/controller/GlobalControllerAdvice.java
@@ -1,18 +1,35 @@
 package org.ngrinder.common.controller;
 
+import org.ngrinder.infra.config.Config;
+import org.ngrinder.user.service.UserContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import static org.ngrinder.common.util.NoOp.noOp;
+
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
 	@Value("${ngrinder.version}")
-	private String nGrinderVersion;
+	private String version;
 
-    @ModelAttribute
+	@Autowired
+	private UserContext userContext;
+
+	@Autowired
+	private Config config;
+
+	@ModelAttribute
     public void globalAttributes(Model model) {
-        model.addAttribute("nGrinderVersion", nGrinderVersion);
-    }
+		model.addAttribute("version", version);
+		model.addAttribute("clustered", config.isClustered());
+		try {
+			model.addAttribute("currentUser", userContext.getCurrentUser());
+		} catch (Exception e) {
+			noOp();
+		}
+	}
 }
--- ngrinder-frontend/src/js/components/common/Copyright.vue
+++ ngrinder-frontend/src/js/components/common/Copyright.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="foot_div">
         <div class="credit">
-            <a href="https://naver.github.io/ngrinder" target="_blank" v-text="`nGrinder v${nGrinderVersion}`"></a>
+            <a href="https://naver.github.io/ngrinder" target="_blank" v-text="`nGrinder v${version}`"></a>
         </div>
     </div>
 </template>
@@ -12,7 +12,7 @@
     @Component({
         name: 'copyright',
         props: {
-            nGrinderVersion: {
+            version: {
                 type: String,
                 required: true,
             },
