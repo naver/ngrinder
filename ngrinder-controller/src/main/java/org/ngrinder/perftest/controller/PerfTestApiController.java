69933ecae19d2cd00b86013eb9669f9d60f61ee9
donggyu-lee <donggyu.lee@nts-corp.com>
2019-03-28 13:33:48 +0900

    Add perftest Searchbar

---
+++ ngrinder-controller/src/main/java/org/ngrinder/perftest/controller/PerfTestApiController.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ngrinder.perftest.controller;
+
+import org.apache.commons.lang.StringUtils;
+import org.ngrinder.common.controller.BaseController;
+import org.ngrinder.common.controller.RestAPI;
+import org.ngrinder.model.User;
+import org.ngrinder.perftest.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Performance Test api Controller.
+ *
+ * @since 3.5.0
+ */
+@RestController
+@RequestMapping("/perftest/api")
+public class PerfTestApiController extends BaseController {
+
+	@Autowired
+	private TagService tagService;
+
+	/**
+	 * Search tags based on the given query.
+	 *
+	 * @param user  user to search
+	 * @param query query string
+	 * @return found tag list in json
+	 */
+	@RestAPI
+	@GetMapping("/search_tag")
+	public List<String> searchTag(User user, @RequestParam(required = false) String query) {
+		List<String> allStrings = tagService.getAllTagStrings(user, query);
+		if (StringUtils.isNotBlank(query)) {
+			allStrings.add(query);
+		}
+		return allStrings;
+	}
+}
--- ngrinder-frontend/src/js/components/Home.vue
+++ ngrinder-frontend/src/js/components/Home.vue
@@ -88,7 +88,7 @@
                     url: this.quickStartUrl,
                     scriptType: this.scriptType,
                 }).then(res => {
-                    // DOTO go to perftest detail.
+                    // TODO Go to perftest detail page.
                 }).catch(error => console.error(error));
             });
         }
