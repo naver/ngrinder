81053796a76d71952e6880dbb4bcf0ffa4379254
donggyu-lee <donggyu.lee@nts-corp.com>
2019-04-23 16:03:47 +0900

    Add utility input components

---
+++ ngrinder-frontend/src/js/components/common/InputPopover.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="input-group input-popover">
+        <input type="text"
+               data-html="true"
+               data-toggle="popover"
+               v-validate="validationRules"
+               :id="name"
+               :name="name"
+               :class="extraCss"
+               :placeholder="placeholder ? placeholder : ''"
+               :title="i18n(message)"
+               :data-content="messageContent ? messageContent : i18n(`${message}.help`)"
+               :data-placement="dataPlacement"
+               :style="customStyle ? customStyle : false"
+               :value="value"
+               @keyup="checkValidation"
+               @input="$emit('input', $event.target.value)"/>
+        <div v-show="errors.has(name)" class="validation-message" v-text="errors.first(name)" :style="errStyle"></div>
+    </div>
+</template>
+
+<script>
+    import Base from '../Base.vue';
+    import Component from 'vue-class-component';
+
+    @Component({
+        name: 'inputPopover',
+        props: {
+            value: {
+                type: [String, Number],
+                required: true,
+            },
+            message: {
+                type: String,
+                required: true,
+            },
+            dataPlacement: {
+                type: String,
+                default: 'bottom',
+            },
+            extraCss: String,
+            name: String,
+            errStyle: String,
+            placeholder: String,
+            messageContent: String,
+            customStyle: String,
+            validationRules: Object,
+        },
+    })
+    export default class InputPopover extends Base {
+        checkValidation() {
+            this.$validator.validateAll().then(result => this.$emit('validationResult', !result));
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ ngrinder-frontend/src/js/components/common/InputPrepend.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="input-prepend-container input-prepend" :class="`input-group ${extraCss}`">
+		<span class="input-group-addon add-on" :title='i18n(message)' v-text="i18n(message)"></span>
+        <input class="form-control input-mini" type="text"
+               :id="name" :name="name" :value="value"
+               @input="$emit('input', $event.target.value)"
+               @change="$emit('change')"
+               aria-describedby="basic-addon1"/>
+    </div>
+</template>
+
+<script>
+    import Base from '../Base.vue';
+    import Component from 'vue-class-component';
+
+    @Component({
+        name: 'inputPrepend',
+        props: {
+            value: {
+                type: [String, Number],
+                required: true,
+            },
+            message: {
+                type: String,
+                required: true,
+            },
+            extraCss: String,
+            name: String,
+        },
+    })
+    export default class InputPrepend extends Base {
+
+    }
+</script>
+
+<style lang="less" scoped>
+    .input-prepend-container {
+        margin-bottom: 0;
+    }
+</style>
