df00207b7bac6e1be501a8a933e866a73e301314
donggyu-lee <donggyu.lee@nts-corp.com>
2019-05-17 11:15:47 +0900

    Implement perftest running tab

--- ngrinder-frontend/src/js/components/common/mixin/ValidationMixin.vue
+++ ngrinder-frontend/src/js/components/common/mixin/ValidationMixin.vue
@@ -2,19 +2,23 @@
     import Base from "../../Base.vue"
     import { Mixin } from 'vue-mixin-decorator'
 
-
     @Mixin
     export default class ValidationMixin extends Base {
         checkValidation() {
-            this.$validator.validateAll().then(result => this.$emit('validationResult', !result))
+            this.$validator.validateAll()
+                .then(() => this.$emit('validationResult', this.errors.any()))
+                .catch(() => this.$emit('validationResult', false));
         }
 
         getCheckValidationPromise() {
             return new Promise((resolve) => {
-                this.$validator.validateAll().then(result => {
-                    this.$emit('validationResult', !result);
+                this.$validator.validateAll().then(() => {
+                    this.$emit('validationResult', this.errors.any());
                     resolve();
-                })
+                }).catch(() => {
+                    this.$emit('validationResult', false);
+                    resolve();
+                });
             });
         }
     }
