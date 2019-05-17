79ede05c1a0c875355b63f62ed8555aef2b8a662
donggyu-lee <donggyu.lee@nts-corp.com>
2019-05-17 11:57:45 +0900

    Initialize data when tab changes in detail

--- ngrinder-frontend/src/js/bus-event.js
+++ ngrinder-frontend/src/js/bus-event.js
@@ -1,6 +1,5 @@
 class Event {
     static get SIGN_UP_FORM_VALIDATION_CHECK() {return 'sign-up-form-validation-check';}
-    static get UPDATE_RAMPUP_CHART() {return 'update-ramp-up-chart';}
     static get RESET_SIGN_UP_MODAL() {return 'reset-sign-up-modal';}
     static get REFRESH_SCRIPT_LIST() {return 'refresh-script-list';}
 }
