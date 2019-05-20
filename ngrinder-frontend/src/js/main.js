5ba16508412c0227802997d92139d4b654fa11f5
imb <nt11294@nts-corp.com>
2019-05-20 16:00:39 +0900

    Implement system config editor

--- ngrinder-frontend/src/js/main.js
+++ ngrinder-frontend/src/js/main.js
@@ -14,6 +14,7 @@ import PerfTestDetail from 'perftest/detail/Detail.vue';
 import ScriptList from 'script/List.vue';
 import ScriptEditor from 'script/Editor.vue';
 import ScriptConsole from 'operation/ScriptConsole.vue';
+import SystemConfig from 'operation/SystemConfig.vue';
 
 import Copyright from 'common/Copyright.vue';
 import Navigator from 'common/navigator/Navigator.vue';
@@ -69,6 +70,7 @@ const routes = [
     {path: '/script/search', component: ScriptList, name: 'scriptSearch'},
     {path: '/script/detail(.*)?', component: ScriptEditor, name: 'scriptEditor'},
     {path: '/operation/script_console', component: ScriptConsole, name: 'scriptConsole'},
+    {path: '/operation/system_config', component: SystemConfig, name: 'systemConfig'},
 ];
 
 const router = new VueRouter({
