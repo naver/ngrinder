05415ebfac732976055ce8cc256043de5affd577
imb <nt11294@nts-corp.com>
2019-05-20 16:00:39 +0900

    Implement system config editor

--- ngrinder-controller/src/test/java/org/ngrinder/operation/SystemConfigControllerTest.java
+++ ngrinder-controller/src/test/java/org/ngrinder/operation/SystemConfigControllerTest.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -9,9 +9,13 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package org.ngrinder.operation;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.ngrinder.AbstractNGrinderTransactionalTest;
@@ -20,11 +24,7 @@ import org.ngrinder.infra.config.Config;
 import org.ngrinder.operation.cotroller.SystemConfigController;
 import org.ngrinder.operation.service.SystemConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ExtendedModelMap;
-import org.springframework.ui.Model;
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import org.springframework.http.HttpEntity;
 
 public class SystemConfigControllerTest extends AbstractNGrinderTransactionalTest {
 
@@ -39,24 +39,22 @@ public class SystemConfigControllerTest extends AbstractNGrinderTransactionalTes
 
 	@Test
 	public void testGetSystemConfiguration() {
-		ExtendedModelMap model = new ExtendedModelMap();
-		controller.getOne(model);
-		assertThat(model.containsAttribute("content"), is(true));
+		String result = controller.getOne().getBody();
+		assertThat(result, notNullValue());
 	}
 
 	@Test
 	public void testSaveSystemConfiguration() {
 		String oriContent = service.getOne();
-		ExtendedModelMap model = new ExtendedModelMap();
 		String content = "test=My test.";
 		try {
-			controller.save(content, model);
+			controller.save(content);
 			ThreadUtils.sleep(2500); //sleep a while to wait for the file monitor to update the system properties.
 			assertThat(service.getOne(), is(content));
 			assertThat(config.getControllerProperties().getProperty("test", ""), is("My test."));
 		} finally {
 			//reset system config
-			controller.save(oriContent, model);
+			controller.save(oriContent);
 		}
 	}
 }
