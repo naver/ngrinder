30db7993d77f3bc6b7f44af3ad0e1c3d33743d16
imb <nt11294@nts-corp.com>
2019-05-21 11:16:22 +0900

    Implement announcement editor

--- ngrinder-frontend/src/js/bus-event.js
+++ ngrinder-frontend/src/js/bus-event.js
@@ -3,6 +3,7 @@ class Event {
     static get UPDATE_RAMPUP_CHART() {return 'update-ramp-up-chart';}
     static get RESET_SIGN_UP_MODAL() {return 'reset-sign-up-modal';}
     static get REFRESH_SCRIPT_LIST() {return 'refresh-script-list';}
+    static get CHANGE_ANNOUNCEMENT() {return 'change-announcement';}
 }
 
 export default Event;
--- ngrinder-frontend/src/js/main.js
+++ ngrinder-frontend/src/js/main.js
@@ -15,6 +15,7 @@ import ScriptList from 'script/List.vue';
 import ScriptEditor from 'script/Editor.vue';
 import ScriptConsole from 'operation/ScriptConsole.vue';
 import SystemConfig from 'operation/SystemConfig.vue';
+import Announcement from 'operation/Announcement.vue';
 
 import Copyright from 'common/Copyright.vue';
 import Navigator from 'common/navigator/Navigator.vue';
@@ -71,6 +72,7 @@ const routes = [
     {path: '/script/detail(.*)?', component: ScriptEditor, name: 'scriptEditor'},
     {path: '/operation/script_console', component: ScriptConsole, name: 'scriptConsole'},
     {path: '/operation/system_config', component: SystemConfig, name: 'systemConfig'},
+    {path: '/operation/announcement', component: Announcement, name: 'announcement'},
 ];
 
 const router = new VueRouter({
