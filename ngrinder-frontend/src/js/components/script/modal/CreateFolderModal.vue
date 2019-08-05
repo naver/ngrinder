c50ba308886618530fc542b776126a9721ac2af2
donggyu-lee <donggyu.lee@nts-corp.com>
2019-08-05 11:18:54 +0900

    Apply bootstrap 4 style to script components

--- ngrinder-frontend/src/js/components/script/modal/CreateFolderModal.vue
+++ ngrinder-frontend/src/js/components/script/modal/CreateFolderModal.vue
@@ -1,33 +1,37 @@
 <template>
     <div id="create-folder-modal" class="modal fade" ref="createFolderModal">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+                <header class="modal-header">
                     <h4 v-text="i18n('script.action.createFolder')"></h4>
-                </div>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+                </header>
                 <div class="modal-body">
-                    <div class="form-horizontal form-horizontal-4">
+                    <div class="form-horizontal">
                         <fieldset>
-                            <control-group :class="{error: errors.has('folderName')}" name="folderName" label-message-key="script.info.folderName" ref="folderNameControlGroup">
+                            <control-group :class="{ error: errors.has('folderName') }" name="folderName"
+                                           label-message-key="script.info.folderName" ref="folderNameControlGroup">
                                 <input type="text"
-                                       class="input-medium"
+                                       class="form-control d-block"
                                        id="folderName"
                                        name="folderName"
-                                       data-placement="right"
+                                       data-placement="bottom"
                                        :data-content="i18n('script.info.folderName.help')"
                                        ref="folderName"
                                        v-model="folderName"
-                                       v-validate="{required: true, regex: '^[a-zA-Z]{1}([a-zA-Z0-9]|[_]|[-]|[.]){2,19}$'}"/>
-                                <span v-show="errors.has('folderName')" class="validation-message" v-text="errors.first('folderName')"></span>
+                                       v-validate="{ required: true, regex: '^[a-zA-Z]{1}([a-zA-Z0-9]|[_]|[-]|[.]){2,19}$' }"/>
                             </control-group>
+                            <span v-show="errors.has('folderName')" class="validation-message mt-1" v-text="errors.first('folderName')"></span>
                         </fieldset>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button class="btn btn-primary" v-text="i18n('common.button.create')" @click="create"></button>
-                    <button class="btn" data-dismiss="modal" v-text="i18n('common.button.cancel')"></button>
-                </div>
+                <footer class="modal-footer">
+                    <button class="btn btn-primary" @click="create">
+                        <i class="fa fa-plus mr-1"></i>
+                        <span v-text="i18n('common.button.create')"></span>
+                    </button>
+                    <button class="btn btn-danger" data-dismiss="modal" v-text="i18n('common.button.cancel')"></button>
+                </footer>
             </div>
         </div>
     </div>
@@ -83,19 +87,36 @@
 
 <style lang="less" scoped>
     #create-folder-modal {
-        display: none;
-
         .control-group {
-            margin-bottom: 20px;
-        }
-    }
+            display: flex;
+            align-items: center;
 
-    input[type="text"] {
-        width: 164px;
-        height: 30px;
+            .controls {
+                margin: 0;
+            }
+
+            input[type="text"] {
+                width: 164px;
+                height: 30px;
+                margin-left: 20px;
+            }
+        }
+
+        .validation-message {
+            margin-left: 161px;
+        }
     }
 
     .modal-body {
         overflow: visible;
+    }
+</style>
+
+<style lang="less">
+    #create-folder-modal {
+        .control-label {
+            width: 140px;
+            text-align: right;
+        }
     }
 </style>
