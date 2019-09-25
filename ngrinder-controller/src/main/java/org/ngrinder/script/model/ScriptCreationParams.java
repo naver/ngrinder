a2b521d6c4db986ca95adc147f89c4d5b0176fcb
donggyu-lee <donggyu.lee@nts-corp.com>
2019-09-25 13:39:26 +0900

    Apply lombok to parameter models

--- ngrinder-controller/src/main/java/org/ngrinder/script/model/ScriptCreationParams.java
+++ ngrinder-controller/src/main/java/org/ngrinder/script/model/ScriptCreationParams.java
@@ -1,61 +1,18 @@
 package org.ngrinder.script.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ScriptCreationParams {
 	private String FileName;
 	private String testUrl;
 	private String options;
 	private String scriptType;
 	private boolean createLibAndResource;
-
-	public ScriptCreationParams() {
-
-	}
-
-	public ScriptCreationParams(String fileName, String testUrl, String options, String scriptType, boolean createLibAndResource) {
-		FileName = fileName;
-		this.testUrl = testUrl;
-		this.options = options;
-		this.scriptType = scriptType;
-		this.createLibAndResource = createLibAndResource;
-	}
-
-	public String getFileName() {
-		return FileName;
-	}
-
-	public void setFileName(String fileName) {
-		FileName = fileName;
-	}
-
-	public String getTestUrl() {
-		return testUrl;
-	}
-
-	public void setTestUrl(String testUrl) {
-		this.testUrl = testUrl;
-	}
-
-	public String getOptions() {
-		return options;
-	}
-
-	public void setOptions(String options) {
-		this.options = options;
-	}
-
-	public String getScriptType() {
-		return scriptType;
-	}
-
-	public void setScriptType(String scriptType) {
-		this.scriptType = scriptType;
-	}
-
-	public boolean isCreateLibAndResource() {
-		return createLibAndResource;
-	}
-
-	public void setCreateLibAndResource(boolean createLibAndResource) {
-		this.createLibAndResource = createLibAndResource;
-	}
 }
