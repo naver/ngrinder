81a6971119d3f80702ac21b67c6ee8dd259f1b9b
imbyungjun <nt11294@nts-corp.com>
2019-09-25 12:23:25 +0900

    Use wrapped CodeMirror vue component

--- ngrinder-frontend/src/js/components/operation/SystemConfig.vue
+++ ngrinder-frontend/src/js/components/operation/SystemConfig.vue
@@ -10,53 +10,24 @@
                 </button>
             </legend>
         </fieldset>
-        <codemirror ref="editor" :options="cmOptions"></codemirror>
+        <code-mirror ref="editor" :options="{ mode: 'properties' }"></code-mirror>
     </div>
 </template>
 
 <script>
     import { Mixins } from 'vue-mixin-decorator';
-    import 'codemirror/mode/properties/properties.js';
-    import 'codemirror/addon/display/fullscreen.js';
-    import 'codemirror/addon/dialog/dialog.js';
-    import 'codemirror/addon/search/search.js';
-    import 'codemirror/addon/selection/active-line.js';
-
-    import { codemirror } from 'vue-codemirror';
     import Component from 'vue-class-component';
     import VueHeadful from 'vue-headful';
 
     import Base from '../Base.vue';
+    import CodeMirror from '../common/CodeMirror.vue';
     import MessagesMixin from '../common/mixin/MessagesMixin.vue';
 
     @Component({
         name: 'systemConfig',
-        components: { codemirror, VueHeadful },
+        components: { CodeMirror, VueHeadful },
     })
     export default class SystemConfig extends Mixins(Base, MessagesMixin) {
-        cmOptions = {
-            mode: 'properties',
-            theme: 'eclipse',
-            line: true,
-            lineNumbers: true,
-            lineWrapping: true,
-            indentUnit: 4,
-            tabSize: 4,
-            indentWithTabs: true,
-            smartIndent: false,
-            visibleTab: true,
-            readOnly: false,
-            styleActiveLine: true,
-            extraKeys: {
-                'F11': function(cm) {
-                    cm.setOption('fullScreen', !cm.getOption('fullScreen'));
-                },
-                'Esc': function(cm) {
-                    if (cm.getOption('fullScreen')) cm.setOption('fullScreen', false);
-                },
-                Tab: 'indentMore',
-            },
-        };
 
         mounted() {
             this.pullSystemConfig();
@@ -88,19 +59,6 @@
 </script>
 
 <style lang="less" scoped>
-    @import '~codemirror/lib/codemirror.css';
-    @import '~codemirror/theme/eclipse.css';
-    @import '~codemirror/addon/display/fullscreen.css';
-    @import '~codemirror/addon/dialog/dialog.css';
-
-    .CodeMirror {
-        border: 1px solid #dddddd;
-    }
-
-    .cm-tab {
-        background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAMCAYAAAAkuj5RAAAAAXNSR0IArs4c6QAAAGFJREFUSMft1LsRQFAQheHPowAKoACx3IgEKtaEHujDjORSgWTH/ZOdnZOcM/sgk/kFFWY0qV8foQwS4MKBCS3qR6ixBJvElOobYAtivseIE120FaowJPN75GMu8j/LfMwNjh4HUpwg4LUAAAAASUVORK5CYII=) no-repeat right;
-    }
-
     button {
         height: 30px;
     }
