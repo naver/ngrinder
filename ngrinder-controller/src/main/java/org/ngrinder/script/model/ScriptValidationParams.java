eb0a3998ba82af2c6cb4348fbad4ca8098db98da
donggyu-lee <donggyu.lee@nts-corp.com>
2019-09-25 13:39:26 +0900

    Apply lombok to parameter models

--- ngrinder-controller/src/main/java/org/ngrinder/script/model/ScriptValidationParams.java
+++ ngrinder-controller/src/main/java/org/ngrinder/script/model/ScriptValidationParams.java
@@ -1,30 +1,15 @@
 package org.ngrinder.script.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ScriptValidationParams {
 	private FileEntry fileEntry;
 	private String hostString;
-
-	public ScriptValidationParams() {
-	}
-
-	public ScriptValidationParams(FileEntry fileEntry, String hostString) {
-		this.fileEntry = fileEntry;
-		this.hostString = hostString;
-	}
-
-	public FileEntry getFileEntry() {
-		return fileEntry;
-	}
-
-	public void setFileEntry(FileEntry fileEntry) {
-		this.fileEntry = fileEntry;
-	}
-
-	public String getHostString() {
-		return hostString;
-	}
-
-	public void setHostString(String hostString) {
-		this.hostString = hostString;
-	}
 }
