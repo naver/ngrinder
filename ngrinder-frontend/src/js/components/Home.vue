0f57c7833cda000469c418d9fe49db07fc4c6fe6
lingjian <lingjian@qudian.com>
2020-03-03 13:12:17 +0800

    fix Index-Developer Resources-More... Open link error

--- ngrinder-frontend/src/js/components/Home.vue
+++ ngrinder-frontend/src/js/components/Home.vue
@@ -18,7 +18,7 @@
                         :seeMoreQuestionUrl="seeMoreQuestionUrl" :askQuestionUrl="askQuestionUrl">
             </home-panel>
             <home-panel :title="i18n('home.resources.title')" :entries="rightPanelEntries" :introJsDataStep="6" :introJsDataIntro="i18n('intro.index.resource')"
-                        :seeMoreQuestionUrl="seeMoreQuestionUrl">
+                        :seeMoreResourcesUrl="seeMoreResourcesUrl">
             </home-panel>
         </section>
         <intro-button></intro-button>
