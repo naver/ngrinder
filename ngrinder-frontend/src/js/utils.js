58c385c4591fa59889346ac449e7d7bcdd8c5977
donggyu.lee <donggyu.lee@nts-corp.com>
2020-07-30 00:16:35 +0900

    Make script comparison correctly

--- ngrinder-frontend/src/js/utils.js
+++ ngrinder-frontend/src/js/utils.js
@@ -12,6 +12,13 @@ class Utils {
     isNumeric(num){
         return !isNaN(num)
     }
+
+    equalsIgnoreNewLineChar(str1, str2) {
+        if (typeof str1 === 'string' && typeof str2 === 'string') {
+            return str1.replace(/\r\n/gm, '\n') === str2.replace(/\r\n/gm, '\n');
+        }
+        throw new TypeError('Arguments are not string');
+    }
 }
 
 export default new Utils();
