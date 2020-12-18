b46314f5c7d19026abcc51e2b8de90eb653f6e13
leedonggyu <donggyu.lee@nts-corp.com>
2020-12-18 17:17:26 +0900

    Increase agent die-hard schedule interval

--- ngrinder-controller/src/main/java/org/ngrinder/perftest/service/samplinglistener/AgentDieHardListener.java
+++ ngrinder-controller/src/main/java/org/ngrinder/perftest/service/samplinglistener/AgentDieHardListener.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -9,7 +9,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package org.ngrinder.perftest.service.samplinglistener;
 
@@ -54,7 +54,7 @@ public class AgentDieHardListener implements SamplingLifeCycleListener, Runnable
 		this.perfTestService = perfTestService;
 		this.agentManager = agentManager;
 		this.scheduledTaskService = scheduledTaskService;
-		this.scheduledTaskService.addFixedDelayedScheduledTask(this, 2);
+		this.scheduledTaskService.addFixedDelayedScheduledTask(this, 2000);
 	}
 
 	@Override
