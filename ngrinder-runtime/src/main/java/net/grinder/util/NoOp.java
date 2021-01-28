8607175d89ffb7b4aca23aa59383098535c13e68
imbyungjun <nt11294@nts-corp.com>
2021-01-28 18:25:20 +0900

    Make grinder utils work on Jython performance test

---
+++ ngrinder-runtime/src/main/java/net/grinder/util/NoOp.java
@@ -0,0 +1,16 @@
+package net.grinder.util;
+
+/**
+ * No operation class.
+ *
+ * @author JunHo Yoon
+ * @since 3.2
+ */
+public abstract class NoOp {
+	/**
+	 * No operation.
+	 */
+	public static void noOp() {
+
+	}
+}
