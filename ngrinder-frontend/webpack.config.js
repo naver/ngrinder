cd12a2853f350ffda113ff9bf552beeab8f1bb24
leedonggyu <donggyu.lee@nts-corp.com>
2021-05-31 17:00:46 +0900

    Bump version up to 3.5.5-p1

--- ngrinder-frontend/webpack.config.js
+++ ngrinder-frontend/webpack.config.js
@@ -35,7 +35,7 @@ var lessLoader = {
 };
 
 module.exports = function (env) {
-    var ngrinderVersion = '3.5.5';
+    var ngrinderVersion = '3.5.5-p1';
     if (env !== undefined && env.ngrinderVersion !== undefined) {
         ngrinderVersion = env.ngrinderVersion;
     }
