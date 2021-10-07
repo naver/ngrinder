f0f5461e4bb15990ddce429b2ae483652b360ac4
leedonggyu <donggyu04@naver.com>
2021-10-07 15:46:13 +0900

    Bump up to 3.5.5-p2

--- ngrinder-frontend/webpack.config.js
+++ ngrinder-frontend/webpack.config.js
@@ -35,7 +35,7 @@ var lessLoader = {
 };
 
 module.exports = function (env) {
-    var ngrinderVersion = '3.5.5-p1';
+    var ngrinderVersion = '3.5.5-p2';
     if (env !== undefined && env.ngrinderVersion !== undefined) {
         ngrinderVersion = env.ngrinderVersion;
     }
