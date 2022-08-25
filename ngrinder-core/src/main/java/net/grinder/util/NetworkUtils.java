771291eb66a8e340d7b1e467945a2f4001e0b1c2
imb <im.b@navercorp.com>
2022-08-25 17:07:53 +0900

    Add InetAddress reachable check and provides a detail error message

--- ngrinder-core/src/main/java/net/grinder/util/NetworkUtils.java
+++ ngrinder-core/src/main/java/net/grinder/util/NetworkUtils.java
@@ -52,6 +52,7 @@ import static org.ngrinder.common.util.NoOp.noOp;
 @SuppressWarnings("SameParameterValue")
 public abstract class NetworkUtils {
 	private static final Logger LOGGER = LoggerFactory.getLogger(NetworkUtils.class);
+	private static final int MAX_REACHABLE_TIMEOUT = 10;
 
 	/**
 	 * Get the local host address, try to get actual IP.
@@ -143,8 +144,7 @@ public abstract class NetworkUtils {
 		return true;
 	}
 
-	static InetAddress getFirstNonLoopbackAddress(boolean preferIpv4, boolean preferIPv6)
-		throws SocketException {
+	static InetAddress getFirstNonLoopbackAddress(boolean preferIpv4, boolean preferIPv6) throws SocketException {
 		Enumeration<?> en = getNetworkInterfaces();
 		while (en.hasMoreElements()) {
 			NetworkInterface i = (NetworkInterface) en.nextElement();
@@ -223,7 +223,13 @@ public abstract class NetworkUtils {
 		if (StringUtils.isNotBlank(ip)) {
 			try {
 				inetAddress = InetAddress.getByName(ip);
-			} catch (Exception e) {
+				if (!inetAddress.isReachable(MAX_REACHABLE_TIMEOUT)) {
+					processException(
+						"Can not check available ports because given local IP address '" + ip + "' is unreachable. " +
+							"Please check the `/etc/hosts` file or manually specify the local IP address in `${NGRINDER_HOME}/system.conf`."
+					);
+				}
+			} catch (SecurityException | IOException e) {
 				noOp();
 			}
 		}
@@ -504,8 +510,7 @@ public abstract class NetworkUtils {
 			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
 			while (interfaces.hasMoreElements()) {
 				NetworkInterface iface = interfaces.nextElement();
-				if (iface.isLoopback() || !iface.isUp())
-					continue;
+				if (iface.isLoopback() || !iface.isUp()) continue;
 
 				Enumeration<InetAddress> addresses = iface.getInetAddresses();
 				while (addresses.hasMoreElements()) {
