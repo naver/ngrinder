85efa4a075354e077a700262ef78e2e9119881bf
Imbyungjun <qudwns2730@naver.com>
2024-02-23 14:40:50 +0900

    Add object input filter on Connector (#1000)
    * Add object input filter on Connector
    
    * Fix ambiguous import
    
    * Change setup jdk to 11

--- ngrinder-runtime/src/main/java/org/ngrinder/dns/LocalManagedDnsImpl.java
+++ ngrinder-runtime/src/main/java/org/ngrinder/dns/LocalManagedDnsImpl.java
@@ -13,7 +13,14 @@
  */
 package org.ngrinder.dns;
 
-import org.xbill.DNS.*;
+import org.xbill.DNS.ARecord;
+import org.xbill.DNS.Cache;
+import org.xbill.DNS.DClass;
+import org.xbill.DNS.Lookup;
+import org.xbill.DNS.PTRRecord;
+import org.xbill.DNS.Record;
+import org.xbill.DNS.TextParseException;
+import org.xbill.DNS.Type;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
