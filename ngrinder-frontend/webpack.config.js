a0809a6577915ad87cbffb0aa625b1814572051b
Imbyungjun <qudwns2730@naver.com>
2024-06-13 16:38:58 +0900

    Bump up to 3.5.9-p1 (#1020)

--- ngrinder-frontend/webpack.config.js
+++ ngrinder-frontend/webpack.config.js
@@ -35,7 +35,7 @@ var lessLoader = {
 };
 
 module.exports = function (env) {
-    var ngrinderVersion = '3.5.9';
+    var ngrinderVersion = '3.5.9-p1';
     if (env !== undefined && env.ngrinderVersion !== undefined) {
         ngrinderVersion = env.ngrinderVersion;
     }
